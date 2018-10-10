ceff7c24394d1344e5598c042949ad1f8d22c590
Justin Boswell <boswej@amazon.com>
2018-10-10 16:30:44 -0700

    Configured cmake to generate classes/headers
    CMake now controls the entire pipeline for generating classes->headers->shared lib

--- source/test.c
+++ source/test.c
@@ -1,5 +1,8 @@
 
-void test()
-{
-    
+#include "aws/jni/com_amazon_aws_Test.h"
+
+#include <stdio.h>
+
+void JNICALL Java_com_amazon_aws_Test_doIt(JNIEnv *env, jclass cls) {
+    printf("I DID THE THING\n");
 }

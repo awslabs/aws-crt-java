4023d442c8aaeaef8bc38e85a60473a6cefc67a8
Justin Boswell <boswej@amazon.com>
2018-10-10 14:41:48 -0700

    Full pipeline test
    Compile class definition in java
    Generate header from class
    Compile using generated header
    Run java test program and load shared lib and call test function

---
+++ tests/com/amazon/aws/test.java
@@ -0,0 +1,40 @@
+
+package com.amazon.aws;
+
+import java.util.StringTokenizer;
+
+class Test implements AutoCloseable {
+    public Test()
+    {
+        String libraryName = "aws-crt-java-test";
+        String cwd = System.getProperty("user.dir");
+        String libraryPath = cwd + "/" + libraryName; 
+        try {
+            System.loadLibrary(libraryName);
+        }
+        catch (java.lang.UnsatisfiedLinkError ex) {
+            System.err.println(ex.toString());
+            String path = System.getProperty("java.library.path");
+            System.err.println("Could not load " + libraryName + " from:");
+            StringTokenizer parser = new StringTokenizer(path, ":");
+            while (parser.hasMoreTokens()) {
+                System.err.println("    " + parser.nextToken());
+            }
+            System.err.println("Current directory: " + cwd);
+        }
+    }
+
+    @Override
+    public void close() {
+        System.out.println("CLOSED");
+    }
+
+    public static native void doIt();
+
+    public static void main(String[] args) {
+        System.out.println("I LIVE");
+        try (Test test = new Test()) {
+            doIt();
+        }
+    }
+};

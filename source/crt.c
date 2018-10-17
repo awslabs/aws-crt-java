4c5f3c339cfe7cecccd8d84894991f73bb55a57d
Justin Boswell <boswej@amazon.com>
2018-10-17 14:32:06 -0700

    Made test code optionally compiled in
    For both java and C

--- source/crt.c
+++ source/crt.c
@@ -18,26 +18,6 @@
 
 #include <stdio.h>
 
-JNIEXPORT
-void JNICALL Java_software_amazon_awssdk_crt_testing_CrtTest_doIt(JNIEnv *env, jobject obj) {
-    (void)env;
-    (void)obj;
-    printf("I DID THE THING\n");
-}
-
-JNIEXPORT 
-void JNICALL Java_software_amazon_awssdk_crt_testing_CrtTest_throwRuntimeExceptionNew(JNIEnv *env, jobject obj) {
-    (void)obj;
-    jclass runtime_exception = (*env)->FindClass(env, "software/amazon/awssdk/crt/CrtRuntimeException");
-    (*env)->ThrowNew(env, runtime_exception, "Test RuntimeException via new");
-}
-
-JNIEXPORT 
-void JNICALL Java_software_amazon_awssdk_crt_testing_CrtTest_throwRuntimeExceptionAPI(JNIEnv *env, jobject obj) {
-    (void)obj;
-    aws_jni_throw_runtime_exception(env, "Test RuntimeException via API");
-}
-
 struct aws_allocator *aws_jni_get_allocator() {
     return aws_default_allocator();
 }
@@ -52,3 +32,25 @@ void aws_jni_throw_runtime_exception(JNIEnv * env, const char *msg) {
 struct aws_byte_cursor aws_jni_byte_cursor_from_jstring(JNIEnv *env, jstring str) {
     return aws_byte_cursor_from_array((*env)->GetStringUTFChars(env, str, NULL), (*env)->GetStringUTFLength(env, str));
 }
+
+#if defined(ENABLE_JNI_TESTS)
+JNIEXPORT
+void JNICALL Java_software_amazon_awssdk_crt_testing_CrtTest_doIt(JNIEnv *env, jobject obj) {
+    (void)env;
+    (void)obj;
+    printf("I DID THE THING\n");
+}
+
+JNIEXPORT
+void JNICALL Java_software_amazon_awssdk_crt_testing_CrtTest_throwRuntimeExceptionNew(JNIEnv *env, jobject obj) {
+    (void)obj;
+    jclass runtime_exception = (*env)->FindClass(env, "software/amazon/awssdk/crt/CrtRuntimeException");
+    (*env)->ThrowNew(env, runtime_exception, "Test RuntimeException via new");
+}
+
+JNIEXPORT
+void JNICALL Java_software_amazon_awssdk_crt_testing_CrtTest_throwRuntimeExceptionAPI(JNIEnv *env, jobject obj) {
+    (void)obj;
+    aws_jni_throw_runtime_exception(env, "Test RuntimeException via API");
+}
+#endif /* ENABLE_JNI_TESTS */

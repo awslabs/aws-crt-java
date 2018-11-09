5c04344f21d7d2999bcadb03c5602e352956b024
Justin Boswell <justin.boswell@gmail.com>
2018-11-08 16:22:37 -0800

    Testing refactor (#17)
    * Created connection fixture, rebased tests on it
    
    * Removed redundant semaphore sharing
    
    * Added test to validate that pub/sub/encoding is working

--- tests/java/PublishTest.java
+++ tests/java/PublishTest.java
@@ -23,49 +23,22 @@ import java.util.function.*;
 import java.nio.ByteBuffer;
 import java.util.concurrent.Semaphore;
 
-public class PublishTest {
+import crt.test.MqttConnectionFixture;
+
+public class PublishTest extends MqttConnectionFixture {
     public PublishTest() {
     }
 
-    static final String TEST_ENDPOINT = "localhost:1883";
-    static final int TEST_TIMEOUT = 3000; /* ms */
     static final String TEST_TOPIC = "publish/me/senpai";
     static final String TEST_PAYLOAD = "PUBLISH ME! SHINY AND CHROME!";
 
     int pubsAcked = 0;
-    boolean disconnecting = false;
 
     @Test
     public void testPublish() {
+        connect();
+        
         try {
-            MqttClient client = new MqttClient(1);
-            assertNotNull(client);
-            assertTrue(client.native_ptr() != 0);
-
-            final Semaphore done = new Semaphore(0);
-
-            MqttConnection.ConnectOptions options = new MqttConnection.ConnectOptions();
-            options.clientId = "PublishTest";
-            options.endpointUri = TEST_ENDPOINT;
-            MqttConnection connection = new MqttConnection(client, options) {
-                @Override
-                public void onOnline() {
-                    done.release();
-                }
-
-                @Override
-                public void onOffline() {
-                    if (!disconnecting) {
-                        fail("Disconnected, server probably hung up");
-                    }
-                    done.release();
-                }
-            };
-
-            connection.connect();
-            done.acquire();
-            assertEquals("Connected", MqttConnection.ConnectionState.Connected, connection.getState());
-
             MqttActionListener pubAck = new MqttActionListener() {
                 @Override
                 public void onSuccess() {
@@ -87,17 +60,12 @@ public class PublishTest {
             done.acquire();
 
             assertEquals("Published", 1, pubsAcked);
-
-            disconnecting = true;
-            connection.disconnect();
-            done.acquire();
-            assertEquals("Disconnected", MqttConnection.ConnectionState.Disconnected, connection.getState());
-        } catch (CrtRuntimeException ex) {
-            fail(ex.getMessage());
         } catch (InterruptedException interrupted) { /* wait() can be interrupted */
             fail(interrupted.getMessage());
         } catch (MqttException mqttEx) {
             fail(mqttEx.getMessage());
         }
+
+        disconnect();
     }
 };

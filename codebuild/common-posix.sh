4ab5e19849533ff7996e92f89b2602a3594f6d2e
Justin Boswell <boswej@amazon.com>
2018-12-06 15:15:33 -0800

    baby's first CI

---
+++ codebuild/common-posix.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+CMAKE_ARGS="$@"
+
+function install_library {
+    git clone https://github.com/awslabs/$1.git
+    cd $1
+
+    if [ -n "$2" ]; then
+        git checkout $2
+    fi
+
+    mkdir build
+    cd build
+
+    cmake -DCMAKE_INSTALL_PREFIX=$AWS_C_INSTALL -DENABLE_SANITIZERS=ON $CMAKE_ARGS ../
+    make install
+
+    cd ../..
+}
+
+cd ../
+
+mkdir -p install
+export AWS_C_INSTALL=`pwd`/install
+
+# If TRAVIS_OS_NAME is OSX, skip this step (will resolve to empty string on CodeBuild)
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
+    sudo apt-get install libssl-dev
+    install_library s2n 7c9069618e68214802ac7fbf45705d5f8b53135f
+fi
+install_library aws-c-common
+install_library aws-c-io
+install_library aws-c-mqtt
+
+cd aws-crt-java
+
+mvn test
---
+++ codebuild/common-windows.bat
@@ -0,0 +1,33 @@
+cd ../
+set CMAKE_ARGS=%*
+
+mkdir install
+set AWS_C_INSTALL=%cd%\\install
+
+CALL :install_library aws-c-common
+CALL :install_library aws-c-io
+CALL :install_library aws-c-mqtt
+
+cd aws-crt-python
+mkdir build
+cd build
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=%AWS_C_INSTALL% ../ || goto error
+cmake --build . --config Release || goto error
+ctest -V || goto error
+
+goto :EOF
+
+:install_library
+git clone https://github.com/awslabs/%~1.git
+cd %~1
+
+if [%~2] == [] GOTO do_build
+git checkout %~2
+
+:do_build
+python3 setup.py build
+exit /b %errorlevel%
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%

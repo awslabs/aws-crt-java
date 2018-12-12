98bf65ea4c36b18af27e37e5a55b18330c2a4e79
Justin Boswell <boswej@amazon.com>
2018-12-11 23:36:08 -0800

    learned to use refreshenv correctly

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -3,7 +3,8 @@ set CMAKE_ARGS=%*
 
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 REM this will also install jdk8
-choco install maven -y && refreshenv
+choco install maven -y
+call RefreshEnv.cmd
 set JAVA_HOME=%~$PATH:javac
 echo JAVA_HOME=%JAVA_HOME%
 

9e6d33934e153615caef17a8049c0e880b09d0a3
Justin Boswell <boswej@amazon.com>
2018-12-11 23:09:15 -0800

    use jdk8 that comes with maven for now

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -2,12 +2,16 @@
 set CMAKE_ARGS=%*
 
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command (New-Object System.Net.WebClient).DownloadFile('https://d3pxv6yz143wms.cloudfront.net/Amazon-Corretto-preview-8u192.msi','C:\tmp\Amazon-Corretto.msi')
-msiexec /i /quiet /Log C:\tmp\Amazon-Corretto.msi
-set JAVA_HOME="C:\Program Files\Amazon Corretto"
-set PATH=%JAVA_HOME%\bin;%PATH%
+REM "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command (New-Object System.Net.WebClient).DownloadFile('https://d3pxv6yz143wms.cloudfront.net/Amazon-Corretto-preview-8u192.msi','C:\tmp\Amazon-Corretto.msi')
+REM msiexec /i /quiet /Log C:\tmp\Amazon-Corretto.msi
+REM set JAVA_HOME="C:\Program Files\Amazon Corretto"
+REM set PATH=%JAVA_HOME%\bin;%PATH%
 REM this will also install jdk8
 choco install maven -y
+refreshenv
+where java > c:\tmp\javapath.txt
+SET /p JAVA_HOME= <c:\tmp\javapath.txt
+echo JAVA_HOME=%JAVA_HOME%
 
 mkdir build\deps\install
 set AWS_C_INSTALL=%cd%\build\deps\install

92e3f03911469378554cc1cfed807366922e3661
Justin Boswell <boswej@amazon.com>
2018-12-12 13:02:38 -0800

    added build_deps.bat for windows, updated common-windows to use it

--- find_vs_cmake_generator.bat
+++ find_vs_cmake_generator.bat
@@ -1,19 +1,19 @@
 @echo off
 
 :: Get a list of Visual Studio generators from cmake
-cmake --help | findstr /R "Visual Studio .+\[arch\]" | for /F "delims=" %%A in ('more') do @(
+for /F "delims=" %%A in ('cmake --help ^| findstr /R "Visual Studio .+\[arch\]"') do @(
     set GENERATOR=%%A
     goto :found
 )
 
 :found
 :: Grab first Visual Studio generator
-set GENERATOR_NAME=""
 for /F "tokens=1 delims==" %%A in ("%GENERATOR%") do (
     set GENERATOR_NAME=%%A
 )
 
 :: Trim leading whitespace
 for /F "tokens=*" %%A in ("%GENERATOR_NAME%") do set TRIMMED=%%A
-set CMAKE_VS_GENERATOR=%TRIMMED:[arch]=Win64%
-echo cmake.generator=%CMAKE_VS_GENERATOR% > mvn-build\cmake.properties
+set CMAKE_VS_GENERATOR=%TRIMMED:[arch] =Win64%
+:: If being run from maven, write out maven properties file
+echo cmake.generator=%CMAKE_VS_GENERATOR%>mvn-build\cmake.properties

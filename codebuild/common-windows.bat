1d635b1f07cdf8f781b2f53e884645c9c2c001c8
Justin Boswell <boswej@amazon.com>
2018-12-11 16:20:51 -0800

    added ninja to deps, x86 build should use i386 java

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,12 +1,12 @@
-cd ../
+
 set CMAKE_ARGS=%*
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 REM this will also install jdk8
 choco install maven -y
 
-mkdir install
-set AWS_C_INSTALL=%cd%\\install
+mkdir build\deps\install
+set AWS_C_INSTALL=%cd%\build\deps\install
 
 CALL :install_library aws-c-common
 CALL :install_library aws-c-io
@@ -25,7 +25,7 @@ if [%~2] == [] GOTO do_build
 git checkout %~2
 
 :do_build
-cmake %CMAKE_ARGS%
+cmake %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX=%AWS_C_INSTALL%
 cmake --build . --target ALL_BUILD
 cmake --build . --target INSTALL
 exit /b %errorlevel%

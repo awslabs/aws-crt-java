96b4450e6ba7ad412835c5198ef0b6caf77bc41e
Justin Boswell <boswej@amazon.com>
2018-12-11 16:01:09 -0800

    switched common-posix.sh to use build-deps.sh

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
 
 CMAKE_ARGS="$@"
 
 function install_library {
+    pushd build/deps/
     git clone https://github.com/awslabs/$1.git
     cd $1
 
@@ -18,23 +20,20 @@ function install_library {
     cmake -DCMAKE_INSTALL_PREFIX=$AWS_C_INSTALL -DENABLE_SANITIZERS=ON $CMAKE_ARGS ../
     make install
 
-    cd ../..
+    popd
 }
 
-cd ../
+mkdir -p `pwd`/build/deps/install
+export AWS_C_INSTALL=`pwd`/build/deps/install
 
-mkdir -p install
-export AWS_C_INSTALL=`pwd`/install
-
-# If TRAVIS_OS_NAME is OSX, skip this step (will resolve to empty string on CodeBuild)
+# Non-OSX unix needs s2n
 if [ "$TRAVIS_OS_NAME" != "osx" ]; then
     sudo apt-get install libssl-dev -y
     install_library s2n 7c9069618e68214802ac7fbf45705d5f8b53135f
 fi
-install_library aws-c-common
-install_library aws-c-io
-install_library aws-c-mqtt transactional-tree
 
-cd aws-crt-java
+# build aws-c-* dependencies
+./build-deps.sh
 
-mvn test
+# build java package
+mvn compile

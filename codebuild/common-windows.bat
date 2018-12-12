47c1f7c7c5bd674ff92d4b66e3a96ce5345a09fd
Justin Boswell <boswej@amazon.com>
2018-12-12 14:26:03 -0800

    use vswhere to find the latest installed visual studio

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -3,7 +3,7 @@ set CMAKE_ARGS=%*
 
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 REM this will also install jdk8
-choco install maven -y
+choco install maven vswhere -y
 call RefreshEnv.cmd
 echo JAVA_HOME=%JAVA_HOME%
 
--- find_vs_cmake_generator.bat
+++ find_vs_cmake_generator.bat
@@ -1,13 +1,21 @@
 @echo off
 
-:: Get a list of Visual Studio generators from cmake
-for /F "delims=" %%A in ('cmake --help ^| findstr /R "Visual Studio .+\[arch\]"') do @(
-    set GENERATOR=%%A
-    goto :found
+:: Get the latest version of Visual Studio
+for /F "delims=" %%A in ('vswhere -legacy -latest -property installationVersion') do @(
+    set VS_VERSION=%%A
 )
 
-:found
-:: Grab first Visual Studio generator
+:: strip minor version info
+for /F "tokens=1 delims=." %%A in ("%VS_VERSION%") do @(
+    set VS_VERSION=%%A
+)
+
+:: Get a list of Visual Studio generators from cmake, match against the version installed
+for /F "delims=" %%A in ('cmake --help ^| findstr /R "Visual Studio %VS_VERSION%.+\[arch\]"') do @(
+    set GENERATOR=%%A
+)
+
+:: Grab the generator name from the the name = description pair
 for /F "tokens=1 delims==" %%A in ("%GENERATOR%") do (
     set GENERATOR_NAME=%%A
 )
@@ -15,5 +23,5 @@ for /F "tokens=1 delims==" %%A in ("%GENERATOR%") do (
 :: Trim leading whitespace
 for /F "tokens=*" %%A in ("%GENERATOR_NAME%") do set TRIMMED=%%A
 set CMAKE_VS_GENERATOR=%TRIMMED:[arch] =Win64%
-:: If being run from maven, write out maven properties file
+:: write out maven properties file
 echo cmake.generator=%CMAKE_VS_GENERATOR%>mvn-build\cmake.properties

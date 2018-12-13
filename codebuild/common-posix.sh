32833141def2148e60dd55276427e02c52ea3a59
Justin Boswell <boswej@amazon.com>
2018-12-12 17:17:48 -0800

    install s2n from fork to test cmake changes

--- codebuild/common-posix.sh
+++ codebuild/common-posix.sh
@@ -7,7 +7,7 @@ CMAKE_ARGS="$@"
 
 function install_library {
     pushd build/deps/
-    git clone https://github.com/awslabs/$1.git
+    git clone https://github.com/$1.git
     cd $1
 
     if [ -n "$2" ]; then
@@ -28,7 +28,7 @@ export AWS_C_INSTALL=`pwd`/build/deps/install
 
 # Non-OSX unix needs s2n
 if [ "$TRAVIS_OS_NAME" != "osx" ]; then
-    install_library s2n 7c9069618e68214802ac7fbf45705d5f8b53135f
+    install_library justinboswell/s2n
 fi
 
 # build aws-c-* dependencies

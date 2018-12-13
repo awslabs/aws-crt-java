c1defcd2379d80fe646975102289f233e1f5906a
Justin Boswell <boswej@amazon.com>
2018-12-13 08:34:15 -0800

    added copyright info to FindJNI.cmake

--- cmake/FindJNI.cmake
+++ cmake/FindJNI.cmake
@@ -1,6 +1,23 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
+# Modified by Amazon: Removed hard dependency on AWT libs, which are not present in a
+# headless install of the JDK.
+#
+# Modifications Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://aws.amazon.com/apache2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+
 #.rst:
 # FindJNI
 # -------

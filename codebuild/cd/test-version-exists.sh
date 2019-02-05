2c23867416f10ef70c5fd05224922ea969ad4013
Justin Boswell <boswej@amazon.com>
2019-02-05 15:19:17 -0800

    Fixed tag detection

--- codebuild/cd/test-version-exists.sh
+++ codebuild/cd/test-version-exists.sh
@@ -6,7 +6,7 @@ git describe --abbrev=0
 #now get the tag
 CURRENT_TAG_VERSION=$(git describe --abbrev=0)
 PUBLISHED_TAG_VERSION=$(curl -s "https://repo.maven.apache.org/maven2/software/amazon/awssdk/crt/aws-crt-java/maven-metadata.xml" | grep "<version>" | tail -1 | cut -f2 -d ">" | cut -f1 -d "<")
-if [[ $PUBLISHED_TAG_VERSION==$CURRENT_TAG_VERSION ]]; then
+if [ $PUBLISHED_TAG_VERSION == $CURRENT_TAG_VERSION ]; then
     echo "$CURRENT_TAG_VERSION is already in Sonatype, cut a new tag if you want to upload another version."
     exit 1
 fi

4c911361a82c6a3fec9b8a360e1604057e6bf11e
Justin Boswell <boswej@amazon.com>
2019-02-06 18:06:41 -0800

    only need 1 windows script, removed commands that muck with the working dir

--- codebuild/cd/windows-vs2015-any.bat
+++ codebuild/cd/windows-vs2015-any.bat
@@ -1,8 +1,6 @@
 
 @setlocal enableextensions enabledelayedexpansion
 
-pushd %~dp0\..\..\
-
  :: install chocolatey
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 :: this will also install jdk8
@@ -12,12 +10,10 @@ echo JAVA_HOME=%JAVA_HOME%
 
 mvn -B compile || goto error
 
-popd
 @endlocal
 goto :EOF
 
 :error
-popd
 @endlocal
 echo Failed with error #%errorlevel%.
 exit /b %errorlevel%

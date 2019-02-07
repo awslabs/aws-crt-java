db4ee15dea32825e37e9a79f3c2c8fc5f5fe3d0d
Justin Boswell <boswej@amazon.com>
2019-02-06 17:57:50 -0800

    fixed working dir on windows

--- codebuild/cd/windows-vs2015-x64.bat
+++ codebuild/cd/windows-vs2015-x64.bat
@@ -1,7 +1,7 @@
 
 @setlocal enableextensions enabledelayedexpansion
 
-pushd %~dp0\..\
+pushd %~dp0\..\..\
 
  :: install chocolatey
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"

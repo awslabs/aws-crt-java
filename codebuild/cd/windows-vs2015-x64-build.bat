896429fd3380387ee468715ee76de606067b31a0
Justin Boswell <boswej@amazon.com>
2019-02-07 15:15:51 -0800

    converted windows build scripts to run in jenkins

---
+++ codebuild/cd/windows-vs2015-x64-build.bat
@@ -0,0 +1,24 @@
+
+@setlocal enableextensions enabledelayedexpansion
+
+ :: install chocolatey
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install jdk8 -y -params "installdir=c:\\jdk8"
+choco install maven -y
+call RefreshEnv.cmd
+echo JAVA_HOME=%JAVA_HOME%
+
+set AWS_CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
+
+mvn -B compile || goto error
+
+mkdir ..\dist
+xcopy /S /F .\mvn-build\lib ..\dist\lib\
+
+@endlocal
+goto :EOF
+
+:error
+@endlocal
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%

80f52fceb896e8ff6ce993ae1fd9af5fb2b68f34
Justin Boswell <boswej@amazon.com>
2019-02-07 11:49:03 -0800

    it wasn't the profile, got rid of cmake upgrade it takes forever and doesnt help

--- codebuild/cd/windows-vs2015-build.bat
+++ codebuild/cd/windows-vs2015-build.bat
@@ -3,10 +3,8 @@
 
  :: install chocolatey
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-:: this will also install jdk8
 choco install jdk8 -y -params "installdir=c:\\jdk8"
 choco install maven -y
-choco upgrade cmake -y --installargs 'ADD_CMAKE_TO_PATH=System'
 call RefreshEnv.cmd
 echo JAVA_HOME=%JAVA_HOME%
 

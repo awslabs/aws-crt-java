ed32e36b64b715880eaca0387f2791ebfbde4123
Justin Boswell <boswej@amazon.com>
2019-02-08 12:09:00 -0800

    cleanly separated vs configuration from cmake wrapping

--- vs_cmake.bat
+++ vs_cmake.bat
@@ -14,29 +14,12 @@ if not exist mvn-build\cmake.properties (
 :: Read generator and Visual Studio version from cmake.properties
 for /f "tokens=2 delims==" %%A in ('type mvn-build\cmake.properties ^| findstr /C:"cmake.generator"') do (
     set GENERATOR=%%A
-    echo Using cached cmake generator from mvn-build\cmake.properties
 )
 for /f "tokens=2 delims==" %%A in ('type mvn-build\cmake.properties ^| findstr /C:"vs.version"') do (
     set VS_VERSION=%%A
 ) 
-
-:: Get the right version of Visual Studio
-if [!VS_VERSION!] EQU [14] (
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
-        set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-    )
-)
-
-if [!VS_VERSION!] EQU [15] (
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
-        set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
-    )
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
-        set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat"
-    ) 
-    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
-        set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
-    )
+for /f "tokens=2 delims==" %%A in ('type mvn-build\cmake.properties ^| findstr /C:"vs.vcvarsall"') do (
+    set VCVARSALL=%%A
 )
 
 if not exist !VCVARSALL! (
@@ -46,7 +29,6 @@ if not exist !VCVARSALL! (
 
 :: If the generator ends with Win64, do a 64 bit build, else 32 bit
 set ISWIN64=!GENERATOR:~-5!
-
 if [%ISWIN64%] == [Win64] (
     set ARCH=amd64
 ) else (

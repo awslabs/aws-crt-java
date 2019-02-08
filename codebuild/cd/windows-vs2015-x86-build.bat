55ad06f61af6d2d7e556b53c1fd011f1662a2865
Justin Boswell <boswej@amazon.com>
2019-02-08 15:02:46 -0800

    quotes

--- codebuild/cd/windows-vs2015-x86-build.bat
+++ codebuild/cd/windows-vs2015-x86-build.bat
@@ -6,7 +6,7 @@ echo JAVA_HOME=%JAVA_HOME%
 
 set AWS_CMAKE_GENERATOR="Visual Studio 14 2015"
 
-mvn -B compile || goto error
+mvn -X compile || goto error
 
 mkdir ..\dist
 xcopy /S /F .\mvn-build\lib ..\dist\lib\

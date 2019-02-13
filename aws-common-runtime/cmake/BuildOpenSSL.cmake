623508cc429a772ae43a5afdc48a92701fbd107f
boswej <boswej@amazon.com>
2019-02-12 17:43:01 -0800

    Fixed up command lines for building openssl via cmake

--- aws-common-runtime/cmake/BuildOpenSSL.cmake
+++ aws-common-runtime/cmake/BuildOpenSSL.cmake
@@ -1,6 +1,6 @@
 
-if($CMAKE_SIZEOF_VOID_P == 4)
-    set(SETARCH_CMD "setarch i386")
+if(${CMAKE_SIZEOF_VOID_P} STREQUAL 4)
+    set(SETARCH_CMD "setarch i386 ")
     set(OPENSSL_C_FLAGS "-m32")
 else()
     set(SETARCH_CMD "")
@@ -8,33 +8,33 @@ else()
 endif()
 
 if("${TARGET_ARCH}" STREQUAL ANDROID)
-    ExternalProject_Add(S2N
-            PREFIX ${AWS_DEPS_BUILD_DIR}
-            URL ${OPENSSL_URL}
-            BUILD_IN_SOURCE 0
-            UPDATE_DISCONNECTED 1
-            CONFIGURE_COMMAND "${SETARCH_CMD} ./config -fPIC  \
-                ${OPENSSL_C_FLAGS} no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib     \
-                no-hw no-mdc2 no-seed no-idea no-camellia\
-                no-bf no-ripemd no-dsa no-ssl2 no-ssl3 no-capieng     \
-                -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS   \
-                --prefix=\"${AWS_DEPS_BUILD_DIR}/openssl\""
-            BUILD_COMMAND "make -j 12"
-            INSTALL_COMMAND "make install_sw"
-            )
+    ExternalProject_Add(OpenSSL
+        PREFIX ${AWS_DEPS_BUILD_DIR}
+        URL ${OPENSSL_URL}
+        BUILD_IN_SOURCE 1
+        UPDATE_DISCONNECTED 1
+        CONFIGURE_COMMAND ${SETARCH_CMD} ./config -fPIC ${OPENSSL_C_FLAGS} 
+            no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 
+            no-seed no-idea no-camellia no-bf no-dsa no-ssl3 no-capieng
+            -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS
+            --prefix=${AWS_DEPS_INSTALL_DIR}
+            --openssldir=${AWS_DEPS_INSTALL_DIR}
+        BUILD_COMMAND "make -j 12"
+        INSTALL_COMMAND "make install_sw"
+        )
 else()
-    ExternalProject_Add(S2N
-            PREFIX ${AWS_DEPS_BUILD_DIR}
-            URL ${OPENSSL_URL}
-            BUILD_IN_SOURCE 0
-            UPDATE_DISCONNECTED 1
-            CONFIGURE_COMMAND "${SETARCH_CMD} ./config -fPIC  \
-                ${OPENSSL_C_FLAGS} no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib     \
-                no-hw no-mdc2 no-seed no-idea no-camellia\
-                no-bf no-ripemd no-dsa no-ssl2 no-ssl3 no-capieng     \
-                -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS   \
-                --prefix=\"${AWS_DEPS_BUILD_DIR}/openssl\""
-            BUILD_COMMAND "make -j 12"
-            INSTALL_COMMAND "make install_sw"
-            )
+    ExternalProject_Add(OpenSSL
+        PREFIX ${AWS_DEPS_BUILD_DIR}
+        URL ${OPENSSL_URL}
+        BUILD_IN_SOURCE 1
+        UPDATE_DISCONNECTED 1
+        CONFIGURE_COMMAND ${SETARCH_CMD} ./config -fPIC ${OPENSSL_C_FLAGS} 
+            no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 
+            no-seed no-idea no-camellia no-bf no-dsa no-ssl3 no-capieng 
+            -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS 
+            --prefix=${AWS_DEPS_INSTALL_DIR} 
+            --openssldir=${AWS_DEPS_INSTALL_DIR}
+        BUILD_COMMAND make -j 12
+        INSTALL_COMMAND make install_sw
+        )
 endif()

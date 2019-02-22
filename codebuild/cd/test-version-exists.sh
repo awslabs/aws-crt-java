18c0c5cef90d70584d41aec0ffa8a7e071b1843f
Justin Boswell <boswej@amazon.com>
2019-02-21 18:06:13 -0800

    fixed version checking so that it should actually work

--- codebuild/cd/test-version-exists.sh
+++ codebuild/cd/test-version-exists.sh
@@ -4,9 +4,9 @@ set -x
 #force a failure if there's no tag
 git describe --abbrev=0
 #now get the tag
-CURRENT_TAG_VERSION=$(git describe --abbrev=0)
-PUBLISHED_TAG_VERSION=$(curl -s "https://repo.maven.apache.org/maven2/software/amazon/awssdk/crt/aws-crt-java/maven-metadata.xml" | grep "<version>" | tail -1 | cut -f2 -d ">" | cut -f1 -d "<")
-if [ $PUBLISHED_TAG_VERSION == $CURRENT_TAG_VERSION ]; then
+CURRENT_TAG_VERSION=$(git describe --abbrev=0 | cut -f2 -dv)
+PUBLISHED_TAG_VERSION=$(curl -s "https://repo.maven.apache.org/maven2/software/amazon/awssdk/crt/aws-crt/maven-metadata.xml" | grep "<latest>" | cut -f2 -d ">" | cut -f1 -d "<")
+if [ "$PUBLISHED_TAG_VERSION" == "$CURRENT_TAG_VERSION" ]; then
     echo "$CURRENT_TAG_VERSION is already in Sonatype, cut a new tag if you want to upload another version."
     exit 1
 fi

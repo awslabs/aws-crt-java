f43922b0e972dd6efe043fb3af320176af675eda
Justin Boswell <boswej@amazon.com>
2019-03-25 16:50:06 -0700

    -d -> -D

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -10,4 +10,4 @@ ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" 
 
 # build java package
 cd $CODEBUILD_SRC_DIR
-mvn -B test -DredirectTestOutputToFile=true -dreuseForks=false -Dendpoint=$ENDPOINT -Dcertificate=/tmp/certificate.pem -Dprivatekey=/tmp/privatekey.pem -Drootca=/tmp/AmazonRootCA1.pem
+mvn -B test -DredirectTestOutputToFile=true -DreuseForks=false -Dendpoint=$ENDPOINT -Dcertificate=/tmp/certificate.pem -Dprivatekey=/tmp/privatekey.pem -Drootca=/tmp/AmazonRootCA1.pem

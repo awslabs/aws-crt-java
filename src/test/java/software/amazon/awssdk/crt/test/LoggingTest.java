04c68536e45e1ca77b650a32918b4be11e8bd7f1
Justin Boswell <boswej@amazon.com>
2019-04-01 10:22:40 -0700

    removed unused import

--- src/test/java/software/amazon/awssdk/crt/test/LoggingTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/LoggingTest.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 

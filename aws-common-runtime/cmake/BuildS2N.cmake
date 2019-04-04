6d77d8abf70b87faebff5d37f679930698b3b81c
Justin Boswell <boswej@amazon.com>
2019-04-03 22:28:27 -0700

    Use fork of s2n until fixes are merged

--- aws-common-runtime/cmake/BuildS2N.cmake
+++ aws-common-runtime/cmake/BuildS2N.cmake
@@ -19,7 +19,7 @@ if("${TARGET_ARCH}" STREQUAL ANDROID)
             -DANDROID_STL=${ANDROID_STL}
             -DENABLE_HW_OPTIMIZATION=OFF
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-            -DUSE_S2N_PQ_CRYTPO=OFF
+            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 else()
     ExternalProject_Add(S2N
@@ -35,6 +35,6 @@ else()
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
-            -DUSE_S2N_PQ_CRYTPO=OFF
+            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 endif()

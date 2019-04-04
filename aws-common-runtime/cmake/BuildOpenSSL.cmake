52075d3f90e1cf29df3657562fa95e826d62d86b
Justin Boswell <boswej@amazon.com>
2019-04-03 17:36:57 -0700

    try disabling CXX

--- aws-common-runtime/cmake/BuildOpenSSL.cmake
+++ aws-common-runtime/cmake/BuildOpenSSL.cmake
@@ -12,10 +12,10 @@ if("${TARGET_ARCH}" STREQUAL ANDROID)
         GIT_TAG ${OPENSSL_SHA}
         BUILD_IN_SOURCE 1
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ./config -fPIC ${OPENSSL_C_FLAGS} 
+        CONFIGURE_COMMAND CXX="" ./config -fPIC ${OPENSSL_C_FLAGS} 
             no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 
             no-seed no-idea no-camellia no-bf no-dsa no-ssl3 no-capieng 
-            no-unit-test no-tests no-buildtest-c++
+            no-unit-test no-tests
             -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS
             --prefix=${AWS_DEPS_INSTALL_DIR}
             --openssldir=${AWS_DEPS_INSTALL_DIR}
@@ -29,10 +29,10 @@ else()
         GIT_TAG ${OPENSSL_SHA}
         BUILD_IN_SOURCE 1
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ./config -fPIC ${OPENSSL_C_FLAGS} 
+        CONFIGURE_COMMAND CXX="" ./config -fPIC ${OPENSSL_C_FLAGS} 
             no-md2 no-rc5 no-rfc3779 no-sctp no-ssl-trace no-zlib no-hw no-mdc2 
             no-seed no-idea no-camellia no-bf no-dsa no-ssl3 no-capieng 
-            no-unit-test no-tests no-buildtest-c++
+            no-unit-test no-tests
             -DSSL_FORBID_ENULL -DOPENSSL_NO_DTLS1 -DOPENSSL_NO_HEARTBEATS 
             --prefix=${AWS_DEPS_INSTALL_DIR} 
             --openssldir=${AWS_DEPS_INSTALL_DIR}

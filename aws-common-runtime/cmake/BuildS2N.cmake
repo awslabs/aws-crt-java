8af256418a5639aae0c839c586d2390a6edce103
Justin Boswell <boswej@amazon.com>
2019-04-03 22:04:27 -0700

    Turned off PQ crypto until openssl can be tamed

--- aws-common-runtime/cmake/BuildS2N.cmake
+++ aws-common-runtime/cmake/BuildS2N.cmake
@@ -19,6 +19,7 @@ if("${TARGET_ARCH}" STREQUAL ANDROID)
             -DANDROID_STL=${ANDROID_STL}
             -DENABLE_HW_OPTIMIZATION=OFF
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 else()
     ExternalProject_Add(S2N
@@ -34,5 +35,6 @@ else()
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 endif()

a5c74d295267372362882a2c26a7174a856e4ed2
Justin Boswell <boswej@amazon.com>
2019-06-20 14:21:12 -0700

    let jenkins do the artifacting

--- codebuild/cd/osx-x64-build.sh
+++ codebuild/cd/osx-x64-build.sh
@@ -5,7 +5,3 @@ set -ex
 cd `dirname $0`/../..
 
 mvn -B compile
-
-# Upload the lib to S3
-GIT_TAG=$(git describe --tags)
-aws s3 cp --recursive target/cmake-build/lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib

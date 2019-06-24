a355aead39ed18d96c3dd8cf7acfe85fdb0a0152
Justin Boswell <justin.boswell@gmail.com>
2019-06-24 10:48:23 -0700

    Use OpenSSL 1.1.1-stable and update S2N revision (#47)
    * Use OpenSSL 1.1.1-stable and newer S2N

--- aws-common-runtime/cmake/BuildS2N.cmake
+++ aws-common-runtime/cmake/BuildS2N.cmake
@@ -19,6 +19,8 @@ if("${TARGET_ARCH}" STREQUAL ANDROID)
             -DANDROID_STL=${ANDROID_STL}
             -DENABLE_HW_OPTIMIZATION=OFF
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+            -DBUILD_TESTING=OFF
+            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 else()
     ExternalProject_Add(S2N
@@ -34,5 +36,7 @@ else()
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
+            -DBUILD_TESTING=OFF
+            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 endif()

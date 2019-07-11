74f8cceef0ae988d8885f24486a6669a022d70df
Justin Boswell <boswej@amazon.com>
2019-07-11 13:55:17 -0700

    Added adoptopenjdk8 to choco install line for windows

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -8,7 +8,7 @@ pushd %~dp0\..\
  :: install chocolatey
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 :: this will also install jdk8
-choco install maven -y
+choco install adoptopenjdk8 maven -y
 call RefreshEnv.cmd
 echo JAVA_HOME=%JAVA_HOME%
 

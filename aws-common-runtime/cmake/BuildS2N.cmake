c2c8d04377113f57986a87f58628b3591f4e3632
Justin Boswell <boswej@amazon.com>
2019-07-15 10:00:31 -0700

    Fixed incorrect usage of BUILD_SHARED_LIBS and pointed S2N at private… (#71)
    * Fixed incorrect usage of BUILD_SHARED_LIBS 
    
    * Pointed at official s2n repo after merge of libcrypto fixes
    
    * Removed unused s2n config flag

--- aws-common-runtime/cmake/BuildS2N.cmake
+++ aws-common-runtime/cmake/BuildS2N.cmake
@@ -20,7 +20,6 @@ if("${TARGET_ARCH}" STREQUAL ANDROID)
             -DENABLE_HW_OPTIMIZATION=OFF
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
             -DBUILD_TESTING=OFF
-            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 else()
     ExternalProject_Add(S2N
@@ -37,6 +36,5 @@ else()
             -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
             -DCMAKE_C_FLAGS=${CMAKE_C_FLAGS}
             -DBUILD_TESTING=OFF
-            -DUSE_S2N_PQ_CRYPTO=OFF
             )
 endif()

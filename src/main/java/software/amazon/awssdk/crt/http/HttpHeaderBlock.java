958f6802bb781d0a1dc5bce2597ca0ebbcfc0115
Justin Boswell <boswej@amazon.com>
2019-09-11 12:55:22 -0700

    Added HttpHeaderBlock enum, fixed copyright on QoS

---
+++ src/main/java/software/amazon/awssdk/crt/http/HttpHeaderBlock.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package software.amazon.awssdk.crt.http;
+
+/**
+ * Quality of Service associated with a publish action or subscription
+ * [MQTT-4.3].
+ */
+public enum HttpHeaderBlock {
+    
+    MAIN(0),
+
+    INFORMATIONAL(1),
+
+    TRAILING(2);
+
+    private int blockType;
+
+    HttpHeaderBlock(int value) {
+        blockType = value;
+    }
+
+    public int getValue() {
+        return blockType;
+    }
+}

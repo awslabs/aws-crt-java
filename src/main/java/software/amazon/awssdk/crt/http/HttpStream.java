265a94e616f32e79f52e08b7142c4f0d662a82dc
Alex Weibel <aweibel@amazon.com>
2019-09-12 15:39:16 -0700

    Preallocate Request Buffers in Buffer Pool

--- src/main/java/software/amazon/awssdk/crt/http/HttpStream.java
+++ src/main/java/software/amazon/awssdk/crt/http/HttpStream.java
@@ -16,6 +16,7 @@
 package software.amazon.awssdk.crt.http;
 
 import software.amazon.awssdk.crt.CrtResource;
+import software.amazon.awssdk.crt.io.CrtByteBuffer;
 
 /**
  * An HttpStream represents a single Http Request/Response pair within a HttpConnection, and wraps the native resources
@@ -24,15 +25,21 @@ import software.amazon.awssdk.crt.CrtResource;
  * Can be used to update the Window size, or to abort the stream early in the middle of sending/receiving Http Bodies.
  */
 public class HttpStream extends CrtResource {
+    private CrtByteBuffer streamBuffer;
 
-    /* Native code can call this constructor */
-    protected HttpStream(long ptr) {
+    /* Native code will call this constructor during HttpConnection.makeRequest() */
+    protected HttpStream(CrtByteBuffer streamBuffer, long ptr) {
+        this.streamBuffer = streamBuffer;
         acquire(ptr);
     }
 
     @Override
     public void close() {
-        httpStreamRelease(release());
+        if (!isNull()) {
+            streamBuffer.releaseBackToPool();
+            streamBuffer = null;
+            httpStreamRelease(release());
+        }
     }
 
     /**
@@ -48,7 +55,9 @@ public class HttpStream extends CrtResource {
         if (windowSize < 0) {
             throw new IllegalArgumentException("windowSize must be >= 0. Actual value: " + windowSize);
         }
-        httpStreamIncrementWindow(native_ptr(), windowSize);
+        if (!isNull()) {
+            httpStreamIncrementWindow(native_ptr(), windowSize);
+        }
     }
 
     /**
@@ -56,7 +65,10 @@ public class HttpStream extends CrtResource {
      * @return The Http Response Status Code
      */
     public int getResponseStatusCode() {
-        return httpStreamGetResponseStatusCode(native_ptr());
+        if (!isNull()) {
+            return httpStreamGetResponseStatusCode(native_ptr());
+        }
+        throw new IllegalStateException("Can't get Status Code on Closed Stream");
     }
 
     private static native void httpStreamRelease(long http_stream);

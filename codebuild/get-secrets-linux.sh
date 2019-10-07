e2f32bc16e007e2a3b492d45a372561e9702f809
Colden Cullen <colden@amazon.com>
2019-10-07 14:46:56 -0700

    Implement new builder support

---
+++ codebuild/get-secrets-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+curl https://www.amazontrust.com/repository/AmazonRootCA1.pem --output /tmp/AmazonRootCA1.pem
+aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\" > /tmp/certificate.pem
+aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\" > /tmp/privatekey.pem
+export ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
+
+mvn -B test -DredirectTestOutputToFile=true -DreuseForks=false -Dendpoint=$ENDPOINT -Dcertificate=/tmp/certificate.pem -Dprivatekey=/tmp/privatekey.pem -Drootca=/tmp/AmazonRootCA1.pem -Daws.crt.debugnative=true

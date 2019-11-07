53b03349a5b65f184847a821c2c7fee3537ad873
Bret Ambrose <bretambrose@gmail.com>
2019-11-07 09:15:00 -0800

    Testing x86 build for tls failures on master

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -4,6 +4,12 @@ set -e
 
 env
 
+# Enable squid-based integration tests
+# These steps are very specific to ubuntu 14.
+
+PROXY_HOST="127.0.0.1"
+PROXY_PORT="3128"
+
 curl https://www.amazontrust.com/repository/AmazonRootCA1.pem --output /tmp/AmazonRootCA1.pem
 cert=$(aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/certificate.pem
 key=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/privatekey.pem
@@ -13,4 +19,6 @@ ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" 
 cd $CODEBUILD_SRC_DIR
 
 ulimit -c unlimited
-mvn -B test -DredirectTestOutputToFile=true -DreuseForks=false -Dendpoint=$ENDPOINT -Dcertificate=/tmp/certificate.pem -Dprivatekey=/tmp/privatekey.pem -Drootca=/tmp/AmazonRootCA1.pem -Daws.crt.debugnative=true
+
+mvn -B test -DredirectTestOutputToFile=true -DreuseForks=false -Dendpoint=$ENDPOINT -Dcertificate=/tmp/certificate.pem -Dprivatekey=/tmp/privatekey.pem -Drootca=/tmp/AmazonRootCA1.pem -Daws.crt.debugnative=true -Dproxyhost=$PROXY_HOST -Dproxyport=$PROXY_PORT
+

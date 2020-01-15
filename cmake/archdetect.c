64dd4d9f269b6f2605af9bb963ba2655f4860d74
Jonathan M. Henson <henso@amazon.com>
2020-01-15 14:20:16 -0800

    Actual working aarch builds this time. (#149)

--- cmake/archdetect.c
+++ cmake/archdetect.c
@@ -3,13 +3,15 @@
 #   error ARCH x86_32
 #elif defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(_M_X64)
 #   error ARCH x86_64
-#elif defined(__arm__) || defined(__TARGET_ARCH_ARM)
-#   if defined(__ARM_ARCH_8__) || (defined(__TARGET_ARCH_ARM) && __TARGET_ARCH_ARM - 0 >= 8)
+#elif defined(__aarch64__) || defined(__arm__)
+#   if defined(__ARM_ARCH) && __ARM_ARCH >= 8
 #       error ARCH armv8
-#   if defined(__ARM_ARCH_7__) || (defined(__TARGET_ARCH_ARM) && __TARGET_ARCH_ARM - 0 >= 7)
+#   elif defined(__ARM_ARCH) && __ARM_ARCH >= 7
 #       error ARCH armv7
-#   elif defined(__ARM_ARCH_6__) || (defined(__TARGET_ARCH_ARM) && __TARGET_ARCH_ARM - 0 >= 6)
+#   elif defined(__ARM_ARCH) && __ARM_ARCH >= 6
 #       error ARCH armv6
 #   endif
+#else
+#    error ARCH unknown
 #endif
-#error ARCH unknown
+

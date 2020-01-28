993e0e03d0208f362254590c813f81a1ce0e4622
Ryan Carper <rccarper@amazon.com>
2020-01-28 19:02:01 +0000

    Potential fix for Raspberry Pi arm issue

--- src/main/java/software/amazon/awssdk/crt/CRT.java
+++ src/main/java/software/amazon/awssdk/crt/CRT.java
@@ -90,7 +90,9 @@ public final class CRT {
             }
         } else if (arch.startsWith("aarch64")) {
            return "armv8";
-        }
+        } else if (arch.equals("arm")) {
+           return "armv7";
+	}
 
         throw new UnknownPlatformException("AWS CRT: architecture not supported: " + arch);
     }

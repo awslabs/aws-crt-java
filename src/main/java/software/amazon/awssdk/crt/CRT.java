1aadcbc83f6afcb6f243ca74d649976c7107f7d6
Justin Boswell <boswej@amazon.com>
2020-02-26 15:58:09 -0800

    Let's see what platform ARM thinks it is

--- src/main/java/software/amazon/awssdk/crt/CRT.java
+++ src/main/java/software/amazon/awssdk/crt/CRT.java
@@ -84,10 +84,8 @@ public final class CRT {
             } else {
                 return "armv6";
             }
-        } else if (arch.startsWith("arm64")) {
-            if (arch.contains("v8")) {
-                return "armv8";
-            }
+        } else if (arch.startsWith("arm64") || arch.startsWith("aarch64")) {
+            return "armv8";
         } else if (arch.startsWith("aarch64")) {
            return "armv8";
         } else if (arch.equals("arm")) {

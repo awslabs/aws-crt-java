6f90d739afcb30c6f0673b6af4d39f89ae010f95
Justin Boswell <boswej@amazon.com>
2020-02-26 16:46:46 -0800

    SYNTAX

--- cmake/archdetect.c
+++ cmake/archdetect.c
@@ -7,7 +7,7 @@
 #elif defined(__aarch64__) 
 #   error ARCH armv8
 #elif defined(__arm__)
-#    if defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__ || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+#    if defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
 #        error ARCH armv7
 #    elif defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__) || defined(__ARM_ARCH_6T2__)
 #        error ARCH armv6

bb3bf4904f3509406012abd494e8462e39abc13e
Justin Boswell <boswej@amazon.com>
2020-03-04 10:54:21 -0800

    Added cross-compile script for CD

---
+++ codebuild/cd/cross-unix-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -ex
+
+[ $# -eq 1 ]
+
+cd `dirname $0`/../..
+
+python3 -c "from urllib.request import urlretrieve; urlretrieve('https://d19elf31gohf1l.cloudfront.net/LATEST/builder.pyz?date=`date +%s`', 'builder')"
+chmod a+x builder
+./builder build -p aws-crt-java --target=$1
+
+# Upload the lib to S3
+GIT_TAG=$(git describe --tags)
+aws s3 cp --recursive target/cmake-build/aws-crt-java/lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib

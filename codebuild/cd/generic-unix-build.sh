8b0c27b3d470872e9b17efbd62201bbb7e48d5b1
Justin Boswell <boswej@amazon.com>
2020-03-18 15:33:50 -0700

    Disabled tests in CD build, removed docker commands from setup (#173)
    * Disabled tests in CD build, removed docker commands from setup

--- codebuild/cd/generic-unix-build.sh
+++ codebuild/cd/generic-unix-build.sh
@@ -10,8 +10,8 @@ AWS_CRT_TARGET=`uname | tr '[:upper:]' '[:lower:]'`-`uname -m`
 
 python3 -c "from urllib.request import urlretrieve; urlretrieve('https://d19elf31gohf1l.cloudfront.net/LATEST/builder.pyz?date=`date +%s`', 'builder')"
 chmod a+x builder
-./builder build -p aws-crt-java --target=$AWS_CRT_TARGET
+./builder build -p aws-crt-java --target=$AWS_CRT_TARGET run_tests=0
 
 # Upload the lib to S3
 GIT_TAG=$(git describe --tags)
-aws s3 cp --recursive target/cmake-build//lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib
+aws s3 cp --recursive target/cmake-build/lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib

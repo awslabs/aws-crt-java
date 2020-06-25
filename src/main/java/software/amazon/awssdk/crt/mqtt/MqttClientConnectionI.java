dc8801d7fa2ce41cf2afc32f32acff0370dd206f
Michael Dombrowski <mdombro@amazon.com>
2020-06-25 14:29:29 -0700

    Add MqttClientConnection as an interface for usage in IoTJobsClient

---
+++ src/main/java/software/amazon/awssdk/crt/mqtt/MqttClientConnectionI.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.awssdk.crt.mqtt;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public interface MqttClientConnectionI {
+    /**
+     * Subscribes to a topic
+     *
+     * @param topic   The topic to subscribe to
+     * @param qos     {@link QualityOfService} for this subscription
+     * @param handler A handler which can receive an MqttMessage when a message is
+     *                published to the topic
+     * @return Future result is the packet/message id associated with the subscribe
+     *         operation
+     */
+    CompletableFuture<Integer> subscribe(String topic, QualityOfService qos, Consumer<MqttMessage> handler);
+
+    /**
+     * Publishes a message to a topic
+     *
+     * @param message The message to publish. The message contains the topic to
+     *                publish to.
+     * @param qos     The {@link QualityOfService} to use for the publish operation
+     * @param retain  Whether or not the message should be retained by the broker to
+     *                be delivered to future subscribers
+     * @return Future value is the packet/message id associated with the publish
+     *         operation
+     */
+    CompletableFuture<Integer> publish(MqttMessage message, QualityOfService qos, boolean retain);
+}

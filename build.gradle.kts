eab5d0656873d51c82fc020d6a27c8b3fdad9739
Justin Boswell <boswej@amazon.com>
2020-08-14 00:23:47 +0000

    copyright

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 /*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
  * Helpful resources/examples:
  * http://gradle.monochromeroad.com/docs/userguide/nativeBinaries.html
  * https://github.com/NationalSecurityAgency/ghidra/blob/master/Ghidra/Features/Decompiler/build.gradle

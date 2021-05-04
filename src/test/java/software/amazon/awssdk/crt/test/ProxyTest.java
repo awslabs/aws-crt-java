3e76a292af453a3b5d84d59c0a276f4cf0f19ddd
Bret Ambrose <bretambrose@gmail.com>
2021-05-04 11:01:13 -0700

    Direct mqtt proxy tests

--- src/test/java/software/amazon/awssdk/crt/test/ProxyTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/ProxyTest.java
@@ -9,9 +9,6 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import software.amazon.awssdk.crt.CRT;
 import software.amazon.awssdk.crt.CrtRuntimeException;
 import software.amazon.awssdk.crt.auth.credentials.Credentials;
@@ -31,11 +28,14 @@ import software.amazon.awssdk.crt.io.HostResolver;
 import software.amazon.awssdk.crt.io.SocketOptions;
 import software.amazon.awssdk.crt.io.TlsContext;
 import software.amazon.awssdk.crt.io.TlsContextOptions;
+import software.amazon.awssdk.crt.mqtt.MqttClient;
+import software.amazon.awssdk.crt.mqtt.MqttClientConnection;
+import software.amazon.awssdk.crt.mqtt.MqttConnectionConfig;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 /*
 
@@ -55,6 +55,9 @@ import java.util.concurrent.ExecutionException;
 # AWS_TEST_X509_ENDPOINT - AWS account-specific endpoint to source x509 credentials from
 # AWS_TEST_X509_THING_NAME - associated name of the x509 thing
 # AWS_TEST_X509_ROLE_ALIAS - associated role alias ...
+
+# AWS_TEST_IOT_SIGNING_REGION - AWS region to make a websocket connection to
+# AWS_TEST_IOT_MQTT_ENDPOINT - AWS account-specific endpoint to connect to IoT core by
 
  */
 public class ProxyTest extends CrtTestFixture  {
@@ -91,6 +94,12 @@ public class ProxyTest extends CrtTestFixture  {
     private static String X509_CREDENTIALS_THING_NAME = System.getenv("AWS_TEST_X509_THING_NAME");
     private static String X509_CREDENTIALS_ROLE_ALIAS = System.getenv("AWS_TEST_X509_ROLE_ALIAS");
 
+    private static String MQTT_WEBSOCKET_REGION = System.getenv("AWS_TEST_IOT_SIGNING_REGION");
+    private static String MQTT_ENDPOINT = System.getenv("AWS_TEST_IOT_MQTT_ENDPOINT");
+
+    private static String PROXY_TEST_CLIENTID = "ProxyTest-";
+    private static final short MQTT_DIRECT_PORT = 8883;
+
     public ProxyTest() {}
 
     private boolean isEnvironmentSetUpForProxyTests() {
@@ -103,6 +112,10 @@ public class ProxyTest extends CrtTestFixture  {
         }
 
         if (X509_CERT_PATH == null || X509_KEY_PATH == null || X509_ROOT_CA_PATH == null || X509_CREDENTIALS_ENDPOINT == null || X509_CREDENTIALS_THING_NAME == null || X509_CREDENTIALS_ROLE_ALIAS == null) {
+            return false;
+        }
+
+        if (MQTT_WEBSOCKET_REGION == null || MQTT_ENDPOINT == null) {
             return false;
         }
 
@@ -377,9 +390,12 @@ public class ProxyTest extends CrtTestFixture  {
         }
     }
 
-    private TlsContext createX509TlsContext() {
+    private TlsContext createX509TlsContext(String alpn) {
         TlsContextOptions options = TlsContextOptions.createWithMtlsFromPath(X509_CERT_PATH, X509_KEY_PATH);
         options.withCertificateAuthorityFromPath(null, X509_ROOT_CA_PATH);
+        if (alpn != null) {
+            options.withAlpnList(alpn);
+        }
 
         return new ClientTlsContext(options);
     }
@@ -388,7 +404,7 @@ public class ProxyTest extends CrtTestFixture  {
         try (EventLoopGroup eventLoopGroup = new EventLoopGroup(1);
              HostResolver resolver = new HostResolver(eventLoopGroup);
              ClientBootstrap bootstrap = new ClientBootstrap(eventLoopGroup, resolver);
-             TlsContext tlsContext = createX509TlsContext();
+             TlsContext tlsContext = createX509TlsContext(null);
              TlsContext proxyTlsContext = createProxyTlsContext(testType)) {
 
             HttpProxyOptions proxyOptions = buildProxyOptions(testType, authType, proxyTlsContext);
@@ -432,6 +448,65 @@ public class ProxyTest extends CrtTestFixture  {
 
         try (CredentialsProvider provider = buildProxiedX509CredentialsProvider(ProxyTestType.TUNNELING_HTTPS, ProxyAuthType.Basic)) {
             doCredentialsProviderProxyTest(provider);
+        }
+    }
+
+    private MqttClientConnection buildDirectMqttConnection(ProxyTestType testType, ProxyAuthType authType) {
+        try (EventLoopGroup eventLoopGroup = new EventLoopGroup(1);
+             HostResolver resolver = new HostResolver(eventLoopGroup);
+             ClientBootstrap bootstrap = new ClientBootstrap(eventLoopGroup, resolver);
+             TlsContext tlsContext = createX509TlsContext(null);
+             MqttClient mqttClient = new MqttClient(bootstrap, tlsContext);
+             MqttConnectionConfig connectionConfig = new MqttConnectionConfig();
+             TlsContext proxyTlsContext = createProxyTlsContext(testType)) {
+
+            HttpProxyOptions proxyOptions = buildProxyOptions(testType, authType, proxyTlsContext);
+            String clientId = PROXY_TEST_CLIENTID + (UUID.randomUUID()).toString();
+
+            connectionConfig.setMqttClient(mqttClient);
+            connectionConfig.setEndpoint(MQTT_ENDPOINT);
+            connectionConfig.setHttpProxyOptions(proxyOptions);
+            connectionConfig.setCleanSession(true);
+            connectionConfig.setClientId(clientId);
+            connectionConfig.setPort(MQTT_DIRECT_PORT);
+            connectionConfig.setProtocolOperationTimeoutMs(60000);
+
+            return new MqttClientConnection(connectionConfig);
+        }
+    }
+
+    private void doMqttConnectTest(MqttClientConnection connection) {
+        CompletableFuture<Boolean> connectFuture = connection.connect();
+        connectFuture.join();
+    }
+
+    @Test
+    public void testMqttDirect_TunnelingProxy_NoAuth() {
+        Assume.assumeTrue(System.getProperty("NETWORK_TESTS_DISABLED") == null);
+        Assume.assumeTrue(isEnvironmentSetUpForProxyTests());
+
+        try (MqttClientConnection connection = buildDirectMqttConnection(ProxyTestType.TUNNELING_HTTPS, ProxyAuthType.None)) {
+            doMqttConnectTest(connection);
+        }
+    }
+
+    @Test
+    public void testMqttDirect_TunnelingProxy_DoubleTls_NoAuth() {
+        Assume.assumeTrue(System.getProperty("NETWORK_TESTS_DISABLED") == null);
+        Assume.assumeTrue(isEnvironmentSetUpForProxyTests());
+
+        try (MqttClientConnection connection = buildDirectMqttConnection(ProxyTestType.TUNNELING_DOUBLE_TLS, ProxyAuthType.None)) {
+            doMqttConnectTest(connection);
+        }
+    }
+
+    @Test
+    public void testMqttDirect_TunnelingProxy_BasicAuth() {
+        Assume.assumeTrue(System.getProperty("NETWORK_TESTS_DISABLED") == null);
+        Assume.assumeTrue(isEnvironmentSetUpForProxyTests());
+
+        try (MqttClientConnection connection = buildDirectMqttConnection(ProxyTestType.TUNNELING_HTTPS, ProxyAuthType.Basic)) {
+            doMqttConnectTest(connection);
         }
     }
 }

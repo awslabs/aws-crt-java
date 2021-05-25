38a331d973f8552bcf424682a32fa56a99fece18
Michael Graeb <graebm@amazon.com>
2021-05-25 16:25:04 -0700

    clang-format

--- src/native/http_request_utils.c
+++ src/native/http_request_utils.c
@@ -23,10 +23,7 @@ struct aws_http_request_body_stream_impl {
     bool body_done;
 };
 
-static int s_aws_input_stream_seek(
-    struct aws_input_stream *stream,
-    int64_t offset,
-    enum aws_stream_seek_basis basis) {
+static int s_aws_input_stream_seek(struct aws_input_stream *stream, int64_t offset, enum aws_stream_seek_basis basis) {
     struct aws_http_request_body_stream_impl *impl = stream->impl;
 
     int result = AWS_OP_SUCCESS;

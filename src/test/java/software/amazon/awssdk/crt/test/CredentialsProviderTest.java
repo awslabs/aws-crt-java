0c729df8a9858eefd300ad0181be19db6606c217
Bret Ambrose <bretambrose@gmail.com>
2021-06-14 19:15:17 -0700

    Profile (#329)
    * add profile credentials provider support
    * Ref count fixup
    
    Co-authored-by: Ian Smith Botsford <issmith@amazon.com>

--- src/test/java/software/amazon/awssdk/crt/test/CredentialsProviderTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/CredentialsProviderTest.java
@@ -5,14 +5,16 @@
 
 package software.amazon.awssdk.crt.test;
 
-import java.lang.InterruptedException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
+
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import software.amazon.awssdk.crt.*;
 import software.amazon.awssdk.crt.auth.credentials.CachedCredentialsProvider;
 import software.amazon.awssdk.crt.auth.credentials.Credentials;
@@ -21,6 +23,7 @@ import software.amazon.awssdk.crt.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.crt.auth.credentials.DefaultChainCredentialsProvider;
 import software.amazon.awssdk.crt.auth.credentials.DelegateCredentialsProvider;
 import software.amazon.awssdk.crt.auth.credentials.DelegateCredentialsHandler;
+import software.amazon.awssdk.crt.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.crt.io.ClientBootstrap;
 import software.amazon.awssdk.crt.io.EventLoopGroup;
 import software.amazon.awssdk.crt.io.HostResolver;
@@ -149,6 +152,89 @@ public class CredentialsProviderTest extends CrtTestFixture {
             assertTrue(Arrays.equals(credentials.getSessionToken(), SESSION_TOKEN.getBytes()));
         } catch (Exception ex) {
             fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testCreateDestroyProfile_ValidCreds() throws IOException {
+        Path confPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_conf_", "");
+        Path credsPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_creds_", "");
+        Files.write(credsPath, Arrays.asList(
+                "[default]",
+                "aws_access_key_id=" + ACCESS_KEY_ID,
+                "aws_secret_access_key=" + SECRET_ACCESS_KEY,
+                "aws_session_token=" + SESSION_TOKEN
+        ));
+
+        ProfileCredentialsProvider.Builder builder = ProfileCredentialsProvider
+                .builder()
+                .withConfigFileNameOverride(confPath.toString())
+                .withCredentialsFileNameOverride(credsPath.toString());
+
+        try (ProfileCredentialsProvider provider = builder.build()) {
+            assertNotNull(provider);
+            assertTrue(provider.getNativeHandle() != 0);
+
+            Credentials credentials = provider.getCredentials().join();
+            assertTrue(Arrays.equals(credentials.getAccessKeyId(), ACCESS_KEY_ID.getBytes()));
+            assertTrue(Arrays.equals(credentials.getSecretAccessKey(), SECRET_ACCESS_KEY.getBytes()));
+            assertTrue(Arrays.equals(credentials.getSessionToken(), SESSION_TOKEN.getBytes()));
+        } finally {
+            Files.deleteIfExists(credsPath);
+            Files.deleteIfExists(confPath);
+        }
+    }
+
+    @Test
+    public void testCreateDestroyProfile_InvalidProfile() throws IOException {
+        Path confPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_conf_", "");
+        Path credsPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_creds_", "");
+
+        ProfileCredentialsProvider.Builder builder = ProfileCredentialsProvider
+                .builder()
+                .withConfigFileNameOverride(confPath.toString())
+                .withCredentialsFileNameOverride(credsPath.toString());
+
+        try {
+            builder.build();
+            fail("Expected builder.build() call to throw exception due to missing [default] section in profile files.");
+        } catch (CrtRuntimeException e) {
+            // Got correct exception, nothing to do
+        } finally {
+            Files.deleteIfExists(confPath);
+            Files.deleteIfExists(credsPath);
+        }
+    }
+
+    @Test
+    public void testCreateDestroyProfile_MissingCreds() throws ExecutionException, InterruptedException, IOException {
+        Path confPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_conf_", "");
+        Path credsPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_creds_", "");
+        Files.write(credsPath, Arrays.asList("[default]")); // Contains a section header but no actual credentials
+
+        ProfileCredentialsProvider.Builder builder = ProfileCredentialsProvider
+                .builder()
+                .withConfigFileNameOverride(confPath.toString())
+                .withCredentialsFileNameOverride(credsPath.toString());
+
+        try (ProfileCredentialsProvider provider = builder.build()) {
+            assertNotNull(provider);
+            assertTrue(provider.getNativeHandle() != 0);
+
+            try {
+                provider.getCredentials().join();
+                fail("Expected credential fetching to throw an exception since creds are missing from profile");
+            } catch (CompletionException e) {
+                assertNotNull(e.getCause());
+                Throwable innerException = e.getCause();
+
+                // Check that the right exception type caused the completion error in the future
+                assertEquals("Failed to get a valid set of credentials", innerException.getMessage());
+                assertEquals(RuntimeException.class, innerException.getClass());
+            }
+        } finally {
+            Files.deleteIfExists(credsPath);
+            Files.deleteIfExists(confPath);
         }
     }
 };

ea0ec75f666521381d8399a5a34547c862582016
Ryan Carper <rccarper@amazon.com>
2021-06-21 09:18:23 -0400

    Adding comment + formatting

--- src/main/java/software/amazon/awssdk/crt/s3/S3ClientNativeCallbacks.java
+++ src/main/java/software/amazon/awssdk/crt/s3/S3ClientNativeCallbacks.java
@@ -2,6 +2,8 @@ package software.amazon.awssdk.crt.s3;
 
 import software.amazon.awssdk.crt.io.StandardRetryOptions;
 
+/* A callback interface for listening to miscellaneous events inside the client. */
 public interface S3ClientNativeCallbacks {
-    default void onSetupStandardRetryOptions(long standardRetryOptionsNativeHandle) { }
+    default void onSetupStandardRetryOptions(long standardRetryOptionsNativeHandle) {
+    }
 }

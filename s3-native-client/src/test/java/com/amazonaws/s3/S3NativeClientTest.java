b27700d69eda4d093cfa44b62e9481093124e1b7
Dengke Tang <dengket@amazon.com>
2021-06-22 15:25:33 -0700

    why it's stil skiped for linux?

--- s3-native-client/src/test/java/com/amazonaws/s3/S3NativeClientTest.java
+++ s3-native-client/src/test/java/com/amazonaws/s3/S3NativeClientTest.java
@@ -52,6 +52,9 @@ public class S3NativeClientTest extends AwsClientTestFixture {
 
     @BeforeClass
     public static void haveAwsCredentials() {
+        if (!areAwsCredentialsAvailable()) {
+            System.out.println(System.getenv("AWS_ACCESS_KEY_ID").equals(""));
+        }
         Assume.assumeTrue(areAwsCredentialsAvailable());
     }
 

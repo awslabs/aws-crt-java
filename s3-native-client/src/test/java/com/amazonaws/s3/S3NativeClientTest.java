3aa6b5eb484326e4796b7ceee747c539f76e989b
Dengke Tang <dengket@amazon.com>
2021-06-22 13:28:55 -0700

    Revert "WIP"
    This reverts commit aa8356e615bb66438aaf0a8c7387125fb2982c92.

--- s3-native-client/src/test/java/com/amazonaws/s3/S3NativeClientTest.java
+++ s3-native-client/src/test/java/com/amazonaws/s3/S3NativeClientTest.java
@@ -86,8 +86,6 @@ public class S3NativeClientTest extends AwsClientTestFixture {
                         public void onException(final CrtRuntimeException e) {
                         }
                     }).join();
-        } catch (CompletionException e) {
-            System.out.print(e);
         }
     }
 

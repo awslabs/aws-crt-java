f30c9ac4895f95dcec19c113024efd010c69a22c
Dengke Tang <dengket@amazon.com>
2021-06-22 14:59:18 -0700

    not a perfect solution but I think it's not bad

--- s3-native-client/src/test/java/com/amazonaws/test/AwsClientTestFixture.java
+++ s3-native-client/src/test/java/com/amazonaws/test/AwsClientTestFixture.java
@@ -7,6 +7,8 @@ package com.amazonaws.test;
 
 import software.amazon.awssdk.crt.auth.credentials.CredentialsProvider;
 import software.amazon.awssdk.crt.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.crt.auth.credentials.DefaultChainCredentialsProvider;
+import software.amazon.awssdk.crt.io.ClientBootstrap;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -14,24 +16,30 @@ import java.nio.charset.StandardCharsets;
  */
 public class AwsClientTestFixture {
     protected static boolean areAwsCredentialsAvailable() {
-        return System.getProperty("crt.aws_access_key_id") != null
-                && !System.getProperty("crt.aws_access_key_id").equals("");
+        return (System.getProperty("crt.aws_access_key_id") != null
+                && !System.getProperty("crt.aws_access_key_id").equals(""))
+                || (System.getenv("AWS_ACCESS_KEY_ID") != null && !System.getenv("AWS_ACCESS_KEY_ID").equals(""));
     }
 
     /**
      * Temporary implementation for local testing
      */
-    protected static CredentialsProvider getTestCredentialsProvider() {
-        final String awsAccessKeyId = System.getProperty("crt.aws_access_key_id");
-        final String awsSecretAccessKey = System.getProperty("crt.aws_secret_access_key");
-        final String awsSessionToken = System.getProperty("crt.aws_session_token");
+    protected static CredentialsProvider getTestCredentialsProvider(ClientBootstrap clientBootstrap) {
+        if (System.getProperty("crt.aws_access_key_id") != null) {
+            final String awsAccessKeyId = System.getProperty("crt.aws_access_key_id");
+            final String awsSecretAccessKey = System.getProperty("crt.aws_secret_access_key");
+            final String awsSessionToken = System.getProperty("crt.aws_session_token");
 
-        StaticCredentialsProvider.StaticCredentialsProviderBuilder builder = new StaticCredentialsProvider.StaticCredentialsProviderBuilder()
-                .withAccessKeyId(awsAccessKeyId.getBytes(StandardCharsets.UTF_8))
-                .withSecretAccessKey(awsSecretAccessKey.getBytes(StandardCharsets.UTF_8));
-        if (awsSessionToken != null && !awsSessionToken.equals("")) {
-            builder.withSessionToken(awsSessionToken.getBytes(StandardCharsets.UTF_8));
+            StaticCredentialsProvider.StaticCredentialsProviderBuilder builder = new StaticCredentialsProvider.StaticCredentialsProviderBuilder()
+                    .withAccessKeyId(awsAccessKeyId.getBytes(StandardCharsets.UTF_8))
+                    .withSecretAccessKey(awsSecretAccessKey.getBytes(StandardCharsets.UTF_8));
+            if (awsSessionToken != null && !awsSessionToken.equals("")) {
+                builder.withSessionToken(awsSessionToken.getBytes(StandardCharsets.UTF_8));
+            }
+            return builder.build();
+        } else {
+            return new DefaultChainCredentialsProvider.DefaultChainCredentialsProviderBuilder()
+                    .withClientBootstrap(clientBootstrap).build();
         }
-        return builder.build();
     }
 }

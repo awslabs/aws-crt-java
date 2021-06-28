a046eb1175254c03a56f33519e05c8c42e5e7e4b
Dengke Tang <dengket@amazon.com>
2021-06-28 15:03:01 -0700

    remove useless stuff

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -8,12 +8,6 @@ import sys
 class AWSCrtJavaTest(Builder.Action):
 
     def run(self, env):
-        # allow custom python to be used
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--python')
-        args = parser.parse_known_args(env.args.args)[0]
-        python = args.python if args.python else sys.executable
-
         # tests must run with leak detection turned on
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
 

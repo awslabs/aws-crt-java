c1b67f468add7e34007bef11b909e17aa197db12
Dengke Tang <dengket@amazon.com>
2021-06-28 15:22:03 -0700

    play around with it

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -1,8 +1,6 @@
 
 import Builder
-import argparse
 import os
-import sys
 
 
 class AWSCrtJavaTest(Builder.Action):
@@ -10,10 +8,8 @@ class AWSCrtJavaTest(Builder.Action):
     def run(self, env):
         # tests must run with leak detection turned on
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
+        os.system( "mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 -DrerunFailingTestsCount=5 && cat log.txt")
 
-        actions = [
-            ['mvn', '-B', 'test', '-DforkCount=0', '-DrerunFailingTestsCount=5'],
-            ['cat', 'log.txt']
-        ]
+        actions = []
 
         return Builder.Script(actions, name='aws-crt-java-test')

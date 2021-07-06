ba3274addc8a258db939e07953360010edc65dfb
Dengke Tang <815825145@qq.com>
2021-07-06 14:18:39 -0700

    Put logs in CI (#346)
    * s3 client test updates and fixes
    
    Co-authored-by: Bret Ambrose <bretambrose@gmail.com>

---
+++ .builder/actions/aws_crt_java_test.py
@@ -0,0 +1,18 @@
+
+import Builder
+import os
+
+
+class AWSCrtJavaTest(Builder.Action):
+
+    def run(self, env):
+        # tests must run with leak detection turned on
+        env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
+        actions = []
+
+        if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 -DrerunFailingTestsCount=5"):
+            # Failed
+            actions.append("exit 1")
+        os.system("cat log.txt")
+
+        return Builder.Script(actions, name='aws-crt-java-test')

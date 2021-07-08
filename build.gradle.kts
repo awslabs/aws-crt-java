ebc18d703023e52542a008b9f786d8b18a4eb9a2
Dengke Tang <dengket@amazon.com>
2021-07-08 10:56:11 -0700

    rename

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,7 @@ description = "software.amazon.awssdk.crt:aws-crt"
 sourceSets {
     main {
         java {
-            setSrcDirs(listOf("src/main/java", 
-                    "${project(":s3-native-client").projectDir}/src/main/java",
-                    "${project(":s3-native-client").projectDir}/src/generated/java"))
+            setSrcDirs(listOf("src/main/java"))
         }
         // include shared libraries built by cmake/CI/CD in the lib folder
         resources {

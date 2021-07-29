a5000eb1d1d29557d21db917593027f144061043
Dengke Tang <dengket@amazon.com>
2021-07-28 18:43:45 -0700

    Revert "momery leak check test"
    This reverts commit 1dfb67ba7e8cfb2c6db653da57276e52f79e1bcf.

--- src/test/java/com/amazonaws/s3/S3NativeClientTest.java
+++ src/test/java/com/amazonaws/s3/S3NativeClientTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
-import java.util.concurrent.Callable;
 
 import com.amazonaws.s3.model.GetObjectOutput;
 import com.amazonaws.s3.model.GetObjectRequest;
@@ -35,14 +34,12 @@ import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import software.amazon.awssdk.crt.CRT;
 import software.amazon.awssdk.crt.CrtRuntimeException;
 import software.amazon.awssdk.crt.s3.*;
 import software.amazon.awssdk.crt.auth.credentials.CredentialsProvider;
 import software.amazon.awssdk.crt.io.*;
 import software.amazon.awssdk.crt.http.HttpHeader;
 import software.amazon.awssdk.crt.http.HttpRequest;
-import software.amazon.awssdk.crt.test.CrtMemoryLeakDetector;
 
 import javax.annotation.processing.Completion;
 
@@ -169,75 +166,57 @@ public class S3NativeClientTest extends AwsClientTestFixture {
     }
 
     @Test
-    public void testConcurrentRequestsWithLeakCheck() {
+    public void testConcurrentRequests() {
         Assume.assumeTrue(System.getProperty("NETWORK_TESTS_DISABLED") == null);
-        Callable<Void> fn = () -> {
+        try (final EventLoopGroup elGroup = new EventLoopGroup(DEFAULT_NUM_THREADS);
+                final HostResolver resolver = new HostResolver(elGroup, DEFAULT_MAX_HOST_ENTRIES);
+                final ClientBootstrap clientBootstrap = new ClientBootstrap(elGroup, resolver);
+                final CredentialsProvider provider = getTestCredentialsProvider();
+                final S3NativeClient nativeClient = new S3NativeClient(REGION, clientBootstrap, provider, 64_000_000l,
+                        10.)) {
 
-            try (final EventLoopGroup elGroup = new EventLoopGroup(DEFAULT_NUM_THREADS);
-                    final HostResolver resolver = new HostResolver(elGroup, DEFAULT_MAX_HOST_ENTRIES);
-                    final ClientBootstrap clientBootstrap = new ClientBootstrap(elGroup, resolver);
-                    final CredentialsProvider provider = getTestCredentialsProvider();
-                    final S3NativeClient nativeClient = new S3NativeClient(REGION, clientBootstrap, provider,
-                            64_000_000l, 10.)) {
+            final long lengthWritten[] = { 0 };
+            final long contentLength = 1024l;
+            final long length[] = { 0 };
+            List<CompletableFuture<?>> futures = new ArrayList<CompletableFuture<?>>();
+            final int concurrentNum = 20;
+            for (int i = 0; i < concurrentNum; i++) {
+                futures.add(
+                        nativeClient.getObject(GetObjectRequest.builder().bucket(BUCKET).key(GET_OBJECT_KEY).build(),
+                                new ResponseDataConsumer<GetObjectOutput>() {
 
-                final long lengthWritten[] = { 0 };
-                final long contentLength = 1024l;
-                final long length[] = { 0 };
-                List<CompletableFuture<?>> futures = new ArrayList<CompletableFuture<?>>();
-                final int concurrentNum = 2;
-                for (int i = 0; i < concurrentNum; i++) {
-                    futures.add(nativeClient.getObject(
-                            GetObjectRequest.builder().bucket(BUCKET).key(GET_OBJECT_KEY).build(),
-                            new ResponseDataConsumer<GetObjectOutput>() {
+                                    @Override
+                                    public void onResponse(GetObjectOutput response) {
+                                        assertNotNull(response);
+                                    }
 
-                                @Override
-                                public void onResponse(GetObjectOutput response) {
-                                    assertNotNull(response);
-                                }
+                                    @Override
+                                    public void onResponseData(ByteBuffer bodyBytesIn) {
+                                        length[0] += bodyBytesIn.remaining();
+                                    }
 
-                                @Override
-                                public void onResponseData(ByteBuffer bodyBytesIn) {
-                                    length[0] += bodyBytesIn.remaining();
-                                }
+                                    @Override
+                                    public void onFinished() {
+                                    }
 
-                                @Override
-                                public void onFinished() {
-                                }
+                                    @Override
+                                    public void onException(final CrtRuntimeException e) {
+                                    }
+                                }));
 
-                                @Override
-                                public void onException(final CrtRuntimeException e) {
-                                }
-                            }));
+                futures.add(nativeClient.putObject(PutObjectRequest.builder().bucket(BUCKET).key(PUT_OBJECT_KEY)
+                        .contentLength(contentLength).build(), buffer -> {
+                            while (buffer.hasRemaining()) {
+                                buffer.put((byte) 42);
+                                ++lengthWritten[0];
+                            }
 
-                    futures.add(nativeClient.putObject(PutObjectRequest.builder().bucket(BUCKET).key(PUT_OBJECT_KEY)
-                            .contentLength(contentLength).build(), buffer -> {
-                                while (buffer.hasRemaining()) {
-                                    buffer.put((byte) 42);
-                                    ++lengthWritten[0];
-                                }
-
-                                return lengthWritten[0] == contentLength;
-                            }));
-                }
-                CompletableFuture<?> allFutures = CompletableFuture
-                        .allOf(futures.toArray(new CompletableFuture<?>[futures.size()]));
-                allFutures.join();
+                            return lengthWritten[0] == contentLength;
+                        }));
             }
-            return null;
-        };
-
-        // Dalvik is SUPER STOCHASTIC about when it frees JVM memory, it has no
-        // observable correlation
-        // to when System.gc() is called. Therefore, we cannot reliably sample it, so we
-        // don't bother.
-        // If we have a leak, we should have it on all platforms, and we'll catch it
-        // elsewhere.
-        if (CRT.getOSIdentifier() != "android") {
-            try {
-                CrtMemoryLeakDetector.leakCheck(fn);
-            } catch (Exception e) {
-                throw new RuntimeException("concurrent test failed!");
-            }
+            CompletableFuture<?> allFutures = CompletableFuture
+                    .allOf(futures.toArray(new CompletableFuture<?>[futures.size()]));
+            allFutures.join();
         }
     }
 

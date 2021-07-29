707e1e66c723c3afb1c23bc9c5252c95d3d634d5
Dengke Tang <dengket@amazon.com>
2021-07-29 14:22:35 -0700

    maybe we can fix it later?

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -26,5 +26,4 @@ mvn -B test $* \
     -Dcertificate=/tmp/certificate.pem \
     -Dprivatekey=/tmp/privatekey.pem \
     -Drootca=/tmp/AmazonRootCA1.pem \
-    -Daws.crt.debugnative=true \
     -Dcmake.s2nNoPqAsm=ON

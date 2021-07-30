130ed139314f5f1aa3b1034dca0df9d103c4276c
Dengke Tang <dengket@amazon.com>
2021-07-30 21:54:10 +0000

    actually we don't need the trace log most of the time

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -11,8 +11,7 @@ class AWSCrtJavaTest(Builder.Action):
         actions = []
 
         if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
-            -DrerunFailingTestsCount=5 -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true\
-            -Daws.crt.aws_trace_log_per_test=true"):
+            -DrerunFailingTestsCount=5 -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
             # Failed
             actions.append("exit 1")
         os.system("cat log.txt")

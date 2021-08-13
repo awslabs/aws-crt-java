27d1f54149fe644209625201d2a744493dec464b
Dengke Tang <dengket@amazon.com>
2021-08-13 13:05:53 -0700

    update

--- codebuild/cd/deploy-snapshot-android.sh
+++ codebuild/cd/deploy-snapshot-android.sh
@@ -1,6 +1,7 @@
 cd ./android
 
 GPG_KEY=$(cat /tmp/aws-sdk-common-runtime.key.asc)
-echo $GPG_KEY
-
-./gradlew -PsigningKey=$"$GPG_KEY" -PsigningPassword=$GPG_PASSPHRASE -PsonatypeUsername='aws-sdk-common-runtime' -PsonatypePassword=$ST_PASSWORD publishToAwsNexus
+# Publish to nexus
+./gradlew -PsigningKey=$"$GPG_KEY" -PsigningPassword=$GPG_PASSPHRASE -PsonatypeUsername='aws-sdk-common-runtime' -PsonatypePassword=$ST_PASSWORD publishToAwsNexus | tee /tmp/android_deploy.log
+# Get the staging repository id and save it
+cat /tmp/android_deploy.log | grep "Created staging repository" | cut -d\' -f2 | tee /tmp/android_repositoryId.txt

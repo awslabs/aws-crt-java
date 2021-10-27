6da168de3db4cd553edb7e8dfc7408d422e6a05f
Dengke Tang <dengket@amazon.com>
2021-10-26 21:06:43 -0700

    I don't like this format

--- src/main/java/software/amazon/awssdk/crt/http/Http2ClientConnection.java
+++ src/main/java/software/amazon/awssdk/crt/http/Http2ClientConnection.java
@@ -26,9 +26,19 @@ public class Http2ClientConnection extends HttpClientConnection {
      * (RFC-7540 7).
      */
     public enum Http2ErrorCode {
-        PROTOCOL_ERROR(1), INTERNAL_ERROR(2), FLOW_CONTROL_ERROR(3), SETTINGS_TIMEOUT(4), STREAM_CLOSED(5),
-        FRAME_SIZE_ERROR(6), REFUSED_STREAM(7), CANCEL(8), COMPRESSION_ERROR(9), CONNECT_ERROR(10),
-        ENHANCE_YOUR_CALM(11), INADEQUATE_SECURITY(12), HTTP_1_1_REQUIRED(13);
+        PROTOCOL_ERROR(1),
+        INTERNAL_ERROR(2),
+        FLOW_CONTROL_ERROR(3),
+        SETTINGS_TIMEOUT(4),
+        STREAM_CLOSED(5),
+        FRAME_SIZE_ERROR(6),
+        REFUSED_STREAM(7),
+        CANCEL(8),
+        COMPRESSION_ERROR(9),
+        CONNECT_ERROR(10),
+        ENHANCE_YOUR_CALM(11),
+        INADEQUATE_SECURITY(12),
+        HTTP_1_1_REQUIRED(13);
 
         private int errorCode;
 

57f1a642909491ec9b6c2cec9b52d2bab01b1c66
Dengke Tang <dengket@amazon.com>
2021-11-04 15:28:35 -0700

    It's working! TIME TO FIND Out how to run java now

--- src/test/java/software/amazon/awssdk/crt/test/Elasticurl.java
+++ src/test/java/software/amazon/awssdk/crt/test/Elasticurl.java
@@ -1,9 +1,15 @@
 package software.amazon.awssdk.crt.test;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.net.URI;
-import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -16,14 +22,23 @@ import org.apache.commons.cli.ParseException;
 import software.amazon.awssdk.crt.Log;
 import software.amazon.awssdk.crt.Log.LogLevel;
 import software.amazon.awssdk.crt.http.HttpVersion;
+import software.amazon.awssdk.crt.http.Http2Request;
+import software.amazon.awssdk.crt.http.HttpClientConnection;
 import software.amazon.awssdk.crt.http.HttpClientConnectionManager;
 import software.amazon.awssdk.crt.http.HttpClientConnectionManagerOptions;
+import software.amazon.awssdk.crt.http.HttpHeader;
+import software.amazon.awssdk.crt.http.HttpRequest;
+import software.amazon.awssdk.crt.http.HttpRequestBase;
+import software.amazon.awssdk.crt.http.HttpRequestBodyStream;
+import software.amazon.awssdk.crt.http.HttpStream;
+import software.amazon.awssdk.crt.http.HttpStreamResponseHandler;
 import software.amazon.awssdk.crt.io.ClientBootstrap;
 import software.amazon.awssdk.crt.io.EventLoopGroup;
 import software.amazon.awssdk.crt.io.HostResolver;
 import software.amazon.awssdk.crt.io.SocketOptions;
 import software.amazon.awssdk.crt.io.TlsContext;
 import software.amazon.awssdk.crt.io.TlsContextOptions;
+import software.amazon.awssdk.crt.utils.ByteBufferUtils;
 
 public class Elasticurl {
 
@@ -40,91 +55,39 @@ public class Elasticurl {
         Options cliOpts = new Options();
 
         cliOpts.addOption("h", "help", false, "show this help message and exit");
-        cliOpts.addOption(Option.builder()
-            .longOpt("cacert")
-            .hasArg()
-            .argName("file")
-            .desc("path to a CA certificate file.")
-            .build());
-        cliOpts.addOption(Option.builder()
-            .longOpt("capath")
-            .hasArg()
-            .argName("dir")
-            .desc("path to a directory containing CA files.")
-            .build());
-        cliOpts.addOption(Option.builder()
-            .longOpt("cert")
-            .hasArg()
-            .argName("file")
-            .desc("path to a PEM encoded certificate to use with mTLS.")
-            .build());
-        cliOpts.addOption(Option.builder()
-            .longOpt("key")
-            .hasArg()
-            .argName("file")
-            .desc("path to a PEM encoded private key that matches cert.")
-            .build());
-        cliOpts.addOption(Option.builder()
-            .longOpt("connect_timeout")
-            .hasArg()
-            .argName("int")
-            .desc("time in milliseconds to wait for a connection.")
-            .build());
-        cliOpts.addOption(Option.builder("H")
-            .longOpt("header")
-            .hasArgs()
-            .argName("str")
-            .desc("line to send as a header in format 'name:value'. May be specified multiple times.")
-            .build());
-        cliOpts.addOption(Option.builder("d")
-            .longOpt("data")
-            .hasArg()
-            .argName("str")
-            .desc("data to send in POST or PUT.")
-            .build());
-        cliOpts.addOption(Option.builder()
-            .longOpt("data_file")
-            .hasArg()
-            .argName("file")
-            .desc("file to send in POST or PUT.")
-            .build());
-        cliOpts.addOption(Option.builder("M")
-            .longOpt("method")
-            .hasArg()
-            .argName("str")
-            .desc("request method. Default is GET)")
-            .build());
+        cliOpts.addOption(Option.builder().longOpt("cacert").hasArg().argName("file")
+                .desc("path to a CA certificate file.").build());
+        cliOpts.addOption(Option.builder().longOpt("capath").hasArg().argName("dir")
+                .desc("path to a directory containing CA files.").build());
+        cliOpts.addOption(Option.builder().longOpt("cert").hasArg().argName("file")
+                .desc("path to a PEM encoded certificate to use with mTLS.").build());
+        cliOpts.addOption(Option.builder().longOpt("key").hasArg().argName("file")
+                .desc("path to a PEM encoded private key that matches cert.").build());
+        cliOpts.addOption(Option.builder().longOpt("connect_timeout").hasArg().argName("int")
+                .desc("time in milliseconds to wait for a connection.").build());
+        cliOpts.addOption(Option.builder("H").longOpt("header").hasArgs().argName("str")
+                .desc("line to send as a header in format 'name:value'. May be specified multiple times.").build());
+        cliOpts.addOption(Option.builder("d").longOpt("data").hasArg().argName("str")
+                .desc("data to send in POST or PUT.").build());
+        cliOpts.addOption(Option.builder().longOpt("data_file").hasArg().argName("file")
+                .desc("file to send in POST or PUT.").build());
+        cliOpts.addOption(Option.builder("M").longOpt("method").hasArg().argName("str")
+                .desc("request method. Default is GET)").build());
         cliOpts.addOption("G", "get", false, "uses GET for request method.");
         cliOpts.addOption("P", "post", false, "uses POST for request method.");
         cliOpts.addOption("I", "head", false, "uses HEAD for request method.");
         cliOpts.addOption("i", "include", false, "includes headers in output.");
         cliOpts.addOption("k", "insecure", false, "turns off x.509 validation.");
-        cliOpts.addOption(Option.builder("o")
-            .longOpt("output")
-            .hasArg()
-            .argName("file")
-            .desc("dumps content-body to FILE instead of stdout.")
-            .build());
-        cliOpts.addOption(Option.builder("t")
-            .longOpt("trace")
-            .hasArg()
-            .argName("file")
-            .desc("dumps logs to FILE instead of stderr.")
-            .build());
-        cliOpts.addOption(Option.builder("p")
-            .longOpt("alpn")
-            .hasArgs()
-            .argName("str")
-            .desc("protocol for ALPN. May be specified multiple times.")
-            .build());
+        cliOpts.addOption(Option.builder("o").longOpt("output").hasArg().argName("file")
+                .desc("dumps content-body to FILE instead of stdout.").build());
+        cliOpts.addOption(Option.builder("t").longOpt("trace").hasArg().argName("file")
+                .desc("dumps logs to FILE instead of stderr.").build());
+        cliOpts.addOption(Option.builder("p").longOpt("alpn").hasArgs().argName("str")
+                .desc("protocol for ALPN. May be specified multiple times.").build());
         cliOpts.addOption(null, "http1_1", false, "HTTP/1.1 connection required.");
         cliOpts.addOption(null, "http2", false, "HTTP/2 connection required.");
-        cliOpts.addOption(Option.builder("v")
-            .longOpt("verbose")
-            .hasArg()
-            .argName("str")
-            .desc("logging level (ERROR|INFO|DEBUG|TRACE) default is none.")
-            .build());
+        cliOpts.addOption(Option.builder("v").longOpt("verbose").hasArg().argName("str")
+                .desc("logging level (ERROR|INFO|DEBUG|TRACE) default is none.").build());
 
         CommandLineParser cliParser = new DefaultParser();
         CommandLine cli = null;
@@ -144,6 +107,80 @@ public class Elasticurl {
         return cli;
     }
 
+    private static HttpRequestBase buildHttpRequest(CommandLine cli, HttpVersion requiredVersion, URI uri)
+            throws Exception {
+        String method = cli.getOptionValue("method");
+        if (cli.hasOption("get")) {
+            method = "GET";
+        } else if (cli.hasOption("post")) {
+            method = "POST";
+        } else if (cli.hasOption("head")) {
+            method = "HEAD";
+        }
+        if (method == null) {
+            method = "GET";
+        }
+        String pathAndQuery = uri.getQuery() == null ? uri.getPath() + "?" + uri.getQuery() : uri.getPath();
+
+        /* body */
+        ByteBuffer tmpPayload = null;
+        if (cli.getOptionValue("data") != null) {
+            tmpPayload = ByteBuffer.wrap(cli.getOptionValue("data").getBytes());
+        } else if (cli.getOptionValue("data_file") != null) {
+            Path path = Paths.get(cli.getOptionValue("data_file"));
+            tmpPayload = ByteBuffer.wrap(Files.readAllBytes(path));
+        }
+        HttpRequestBodyStream payloadStream = null;
+        if (tmpPayload != null) {
+            final ByteBuffer payload = tmpPayload;
+            payloadStream = new HttpRequestBodyStream() {
+                @Override
+                public boolean sendRequestBody(ByteBuffer outBuffer) {
+                    ByteBufferUtils.transferData(payload, outBuffer);
+                    return payload.remaining() == 0;
+                }
+
+                @Override
+                public boolean resetPosition() {
+                    return true;
+                }
+
+                @Override
+                public long getLength() {
+                    return payload.capacity();
+                }
+            };
+        }
+        /* initial headers */
+        HttpHeader[] headers = new HttpHeader[] {};
+        HttpRequestBase request = requiredVersion == HttpVersion.HTTP2 ? new Http2Request(headers, payloadStream)
+                : new HttpRequest(method, pathAndQuery, headers, payloadStream);
+
+        /* Version specific headers */
+        if (requiredVersion == HttpVersion.HTTP2) {
+            request.addHeader(new HttpHeader(":method", method));
+            request.addHeader(new HttpHeader(":scheme", uri.getScheme()));
+            request.addHeader(new HttpHeader(":authority", uri.getAuthority()));
+            request.addHeader(new HttpHeader(":path", pathAndQuery));
+        } else {
+            request.addHeader(new HttpHeader("Host", uri.getHost()));
+        }
+
+        /* General headers */
+        request.addHeader(new HttpHeader("accept", "*/*"));
+        request.addHeader(new HttpHeader("user-agent", "elasticurl 1.0, Powered by the AWS Common Runtime."));
+
+        /* Customized headers */
+        String[] customizedHeaders = cli.getOptionValues("header");
+        if (customizedHeaders != null) {
+            for (String header : customizedHeaders) {
+                String[] pair = header.split(":");
+                request.addHeader(new HttpHeader(pair[0], pair[1]));
+            }
+        }
+        return request;
+    }
+
     public static void main(String args[]) throws Exception {
         CommandLine cli = parseArgs(args);
 
@@ -151,13 +188,13 @@ public class Elasticurl {
         String verbose = cli.getOptionValue("verbose");
         if (verbose != null) {
             LogLevel logLevel = LogLevel.None;
-            if (verbose == "ERROR") {
+            if (verbose.equals("ERROR")) {
                 logLevel = LogLevel.Error;
-            } else if (verbose == "INFO") {
+            } else if (verbose.equals("INFO")) {
                 logLevel = LogLevel.Info;
-            } else if (verbose == "DEBUG") {
+            } else if (verbose.equals("DEBUG")) {
                 logLevel = LogLevel.Debug;
-            } else if (verbose == "TRACE") {
+            } else if (verbose.equals("TRACE")) {
                 logLevel = LogLevel.Trace;
             } else {
                 exit(logLevel + " unsupported value for verbose option");
@@ -216,11 +253,11 @@ public class Elasticurl {
                 String[] alpn = cli.getOptionValues("alpn");
                 if (alpn == null) {
                     if (requiredVersion == HttpVersion.HTTP1_1) {
-                        alpn = new String[]{"http/1.1"};
+                        alpn = new String[] { "http/1.1" };
                     } else if (requiredVersion == HttpVersion.HTTP2) {
-                        alpn = new String[]{"h2"};
+                        alpn = new String[] { "h2" };
                     } else {
-                        alpn = new String[]{"h2", "http/1.1"};
+                        alpn = new String[] { "h2", "http/1.1" };
                     }
                 }
                 tlsOpts.alpnList = Arrays.asList(alpn);
@@ -228,28 +265,79 @@ public class Elasticurl {
                 tlsCtx = new TlsContext(tlsOpts);
             }
 
-            CompletableFuture connMgrShutdownComplete = null;
-
+            CompletableFuture<Void> connMgrShutdownComplete = null;
+            final BufferedOutputStream out = cli.getOptionValue("output") == null ? new BufferedOutputStream(System.out)
+                    : new BufferedOutputStream(new FileOutputStream(cli.getOptionValue("output")));
             try (EventLoopGroup eventLoopGroup = new EventLoopGroup(1);
-                 HostResolver resolver = new HostResolver(eventLoopGroup);
-                 ClientBootstrap bootstrap = new ClientBootstrap(eventLoopGroup, resolver);
-                 SocketOptions socketOpts = new SocketOptions()) {
-
+                    HostResolver resolver = new HostResolver(eventLoopGroup);
+                    ClientBootstrap bootstrap = new ClientBootstrap(eventLoopGroup, resolver);
+                    SocketOptions socketOpts = new SocketOptions()) {
+                if (cli.getOptionValue("connect_timeout") != null) {
+                    int timeout = Integer.parseInt(cli.getOptionValue("connect_timeout"));
+                    socketOpts.connectTimeoutMs = timeout;
+                }
                 HttpClientConnectionManagerOptions connMgrOpts = new HttpClientConnectionManagerOptions()
-                    .withClientBootstrap(bootstrap)
-                    .withSocketOptions(socketOpts)
-                    .withTlsContext(tlsCtx)
-                    .withUri(uri)
-                    .withPort(port);
+                        .withClientBootstrap(bootstrap).withSocketOptions(socketOpts).withTlsContext(tlsCtx)
+                        .withUri(uri).withPort(port);
 
                 try (HttpClientConnectionManager connMgr = HttpClientConnectionManager.create(connMgrOpts)) {
                     connMgrShutdownComplete = connMgr.getShutdownCompleteFuture();
-                    try () { YOU ARE HERE
+                    try (HttpClientConnection conn = connMgr.acquireConnection().get(60, TimeUnit.SECONDS)) {
+
+                        final CompletableFuture<Void> reqCompleted = new CompletableFuture<>();
+                        HttpStreamResponseHandler streamHandler = new HttpStreamResponseHandler() {
+                            boolean statusWritten = false;
+
+                            @Override
+                            public void onResponseHeaders(HttpStream stream, int responseStatusCode, int blockType,
+                                    HttpHeader[] nextHeaders) {
+                                if (blockType == 1) {
+                                    /* Ignore informational headers */
+                                    return;
+                                }
+                                if (cli.hasOption("include")) {
+                                    if (!statusWritten) {
+                                        System.out.println(String.format("Response Status: %d", responseStatusCode));
+                                        statusWritten = true;
+                                    }
+                                    for (HttpHeader header : nextHeaders) {
+                                        System.out.println(header.getName() + ": " + header.getValue());
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public int onResponseBody(HttpStream stream, byte[] bodyBytesIn) {
+                                try {
+                                    out.write(bodyBytesIn, 0, bodyBytesIn.length);
+                                    out.flush();
+                                } catch (Exception e) {
+                                    exit("Failed to write the body");
+                                }
+                                return bodyBytesIn.length;
+                            }
+
+                            @Override
+                            public void onResponseComplete(HttpStream stream, int errorCode) {
+                                reqCompleted.complete(null);
+                                stream.close();
+                            }
+                        };
+                        HttpRequestBase request = buildHttpRequest(cli, requiredVersion, uri);
+                        HttpStream stream = conn.makeRequest(request, streamHandler);
+                        stream.activate();
+
+                        // Give the request up to 60 seconds to complete, otherwise throw a
+                        // TimeoutException
+                        reqCompleted.get(60, TimeUnit.SECONDS);
                     }
-                } finally {
-                    if (connMgrShutdownComplete != null) {
-                        connMgrShutdownComplete.get();
-                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            } finally {
+                out.close();
+                if (connMgrShutdownComplete != null) {
+                    connMgrShutdownComplete.get();
                 }
             }
 

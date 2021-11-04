13945b145dc8ace5a665f7eab24ff486f37da4a7
Dengke Tang <dengket@amazon.com>
2021-11-04 16:28:05 -0700

    let's see

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -10,19 +10,25 @@ class AWSCrtJavaTest(Builder.Action):
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
         actions = []
 
-        if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
-            -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
-            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
-            # Failed
-            actions.append("exit 1")
-        os.system("cat log.txt")
+        # if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
+        #     -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
+        #     -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
+        #     # Failed
+        #     actions.append("exit 1")
+        # os.system("cat log.txt")
+        # Three level up of the currect file path .builder/actions/__FILE__
+        source_path = os.path.dirname(
+            os.path.dirname((os.path.dirname(__file__))))
+        home_dir = os.path.expanduser("~")
+        print(source_path)
         actions.append(
             ['java',
              '-classpath',
-             '\{source_dir\}/target/test-classes:\{source_dir\}/target/classes:~/.m2/repository/commons-cli/commons-cli/1.4/commons-cli-1.4.jar',
+             '{source}/target/test-classes:{source}/target/classes:{home}/.m2/repository/commons-cli/commons-cli/1.4/commons-cli-1.4.jar'.format(
+                 source=source_path, home=home_dir),
              'software.amazon.awssdk.crt.test.Elasticurl',
              '-v',
-             'TRACE',
+             'ERROR',
              '--http2',
              '-o',
              'elastigirl_h2.png',

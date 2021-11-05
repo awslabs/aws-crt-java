958f8a9ace48db790c525dfec8cd44b018f27004
Dengke Tang <dengket@amazon.com>
2021-11-05 10:39:17 -0700

    update the script

--- .builder/actions/java_elasticurl_runner.py
+++ .builder/actions/java_elasticurl_runner.py
@@ -1,10 +1,44 @@
 import sys
-import os
+import subprocess
+from subprocess import Popen, PIPE
 
+TIMEOUT = 100
 # Runner for elasticurl integration tests
 elasticurl_args = sys.argv[1:]
 
-java_command = "mvn -e exec:java -Dexec.classpathScope=\"test\" -Dexec.mainClass=\"software.amazon.awssdk.crt.test.Elasticurl\" -Dexec.args=\"{args}\"".format(
-    args=" ".join(elasticurl_args))
+mvn_args = " ".join(elasticurl_args)
 
-os.system(java_command)
+java_command = ['mvn', '-e', 'exec:java', '-Dexec.classpathScope=\"test\"',
+                '-Dexec.mainClass=\"software.amazon.awssdk.crt.test.Elasticurl\"', '-Dexec.args=\"{}\"'.format(mvn_args)]
+
+print(java_command)
+
+command_string = " ".join(java_command)
+print(command_string)
+
+
+def run_command(args_str):
+    # gather all stderr and stdout to a single string that we print only if things go wrong
+    process = subprocess.Popen(
+        args_str, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    timedout = False
+    try:
+        output = process.communicate(timeout=TIMEOUT)[0]
+    except subprocess.TimeoutExpired:
+        timedout = True
+        process.kill()
+        output = process.communicate()[0]
+    finally:
+        if process.returncode != 0 or timedout:
+            print(args_str)
+            for line in output.splitlines():
+                print(line.decode())
+            if timedout:
+                raise RuntimeError("Timeout happened after {secs} secs from: {cmd}".format(
+                    secs=TIMEOUT, cmd=args_str))
+            else:
+                raise RuntimeError("Return code {code} from: {cmd}".format(
+                    code=process.returncode, cmd=args_str))
+
+
+run_command(command_string)

50a501ab9cc8e8371774c99c917536bee85cd0da
Dengke Tang <dengket@amazon.com>
2021-11-08 10:12:40 -0800

    why it's failling harder

--- integration-testing/java_elasticurl_runner.py
+++ integration-testing/java_elasticurl_runner.py
@@ -22,13 +22,13 @@ java_command = ['mvn', '-e', 'exec:java', '-Dexec.classpathScope=\"test\"',
 
 command_string = " ".join(java_command)
 
-args = shlex.split(command_string)
+# args = shlex.split(command_string)
 
 
-def run_command(args):
+def run_command(args_str):
     # gather all stderr and stdout to a single string that we print only if things go wrong
     process = subprocess.Popen(
-        args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        args_str, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     timedout = False
     try:
         output = process.communicate(timeout=TIMEOUT)[0]
@@ -37,7 +37,7 @@ def run_command(args):
         process.kill()
         output = process.communicate()[0]
     finally:
-        args_str = subprocess.list2cmdline(args)
+        # args_str = subprocess.list2cmdline(args)
         if process.returncode != 0 or timedout:
             print(args_str)
             for line in output.splitlines():
@@ -50,4 +50,4 @@ def run_command(args):
                     code=process.returncode, cmd=args_str))
 
 
-run_command(args)
+run_command(command_string)

9f496a44d15b8066156221513a4c094bc7cedddf
Dengke Tang <dengket@amazon.com>
2021-11-12 16:05:31 -0800

    Revert "try not use shell?"
    This reverts commit 8c5068bfada03bbdb589c874586b9d1688a9ab21.

--- integration-testing/java_elasticurl_runner.py
+++ integration-testing/java_elasticurl_runner.py
@@ -11,14 +11,17 @@ mvn_args = " ".join(map(shlex.quote, sys.argv[1:]))
 
 java_command = ['mvn', '-e', 'exec:java', '-Dexec.classpathScope=\"test\"',
                 '-Dexec.mainClass=\"software.amazon.awssdk.crt.test.Elasticurl\"', '-Dexec.args=\"{}\"'.format(mvn_args)]
-
+print(java_command)
 if os.name == 'nt':
     java_command[0] = 'mvn.cmd'
+command_string = " ".join(java_command)
 
-def run_command(args):
+
+def run_command(args_str):
+    print(args_str)
     # gather all stderr and stdout to a single string that we print only if things go wrong
     process = subprocess.Popen(
-        args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        args_str, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     timedout = False
     try:
         output = process.communicate(timeout=TIMEOUT)[0]
@@ -28,7 +31,7 @@ def run_command(args):
         output = process.communicate()[0]
     finally:
         if process.returncode != 0 or timedout:
-            args_str = subprocess.list2cmdline(args)
+            print(args_str)
             for line in output.splitlines():
                 print(line.decode())
             if timedout:
@@ -39,4 +42,4 @@ def run_command(args):
                     code=process.returncode, cmd=args_str))
 
 
-run_command(java_command)
+run_command(command_string)

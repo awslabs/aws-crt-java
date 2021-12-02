24be4c8f7d684901d57d15be92a40345a5c20d1c
Dengke Tang <dengket@amazon.com>
2021-12-01 17:01:04 -0800

    revert the formatting change

--- src/main/java/software/amazon/awssdk/crt/http/HttpClientConnection.java
+++ src/main/java/software/amazon/awssdk/crt/http/HttpClientConnection.java
@@ -6,76 +6,72 @@
 package software.amazon.awssdk.crt.http;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.Map;
+import java.util.HashMap;
 
 import software.amazon.awssdk.crt.CRT;
 import software.amazon.awssdk.crt.CrtResource;
 import software.amazon.awssdk.crt.CrtRuntimeException;
+import software.amazon.awssdk.crt.http.HttpStreamResponseHandler;
+import software.amazon.awssdk.crt.http.HttpStream;
 
 import static software.amazon.awssdk.crt.CRT.awsLastError;
 
 /**
- * This class wraps aws-c-http to provide the basic HTTP request/response
- * functionality via the AWS Common Runtime.
+ * This class wraps aws-c-http to provide the basic HTTP request/response functionality via the AWS Common Runtime.
  *
- * HttpClientConnection represents a single connection to a HTTP service
- * endpoint.
+ * HttpClientConnection represents a single connection to a HTTP service endpoint.
  *
- * This class is not thread safe and should not be called from different
- * threads.
+ * This class is not thread safe and should not be called from different threads.
  */
 public class HttpClientConnection extends CrtResource {
+
     protected HttpClientConnection(long connectionBinding) {
         acquireNativeHandle(connectionBinding);
     }
 
     /**
-     * Schedules an HttpRequest on the Native EventLoop for this
-     * HttpClientConnection.
+     * Schedules an HttpRequest on the Native EventLoop for this HttpClientConnection.
      *
-     * @param request       The Request to make to the Server.
-     * @param streamHandler The Stream Handler to be called from the Native
-     *                      EventLoop
+     * @param request The Request to make to the Server.
+     * @param streamHandler The Stream Handler to be called from the Native EventLoop
      * @throws CrtRuntimeException if stream creation fails
-     * @return The HttpStream that represents this Request/Response Pair. It can be
-     *         closed at any time during the request/response, but must be closed by
-     *         the user thread making this request when it's done.
+     * @return The HttpStream that represents this Request/Response Pair. It can be closed at any time during the
+     *          request/response, but must be closed by the user thread making this request when it's done.
      */
     public HttpStream makeRequest(HttpRequestBase request, HttpStreamResponseHandler streamHandler)
             throws CrtRuntimeException {
         if (isNull()) {
             throw new IllegalStateException("HttpClientConnection has been closed, can't make requests on it.");
         }
-        HttpStream stream = httpClientConnectionMakeRequest(getNativeHandle(), request.marshalForJni(),
-                request.getBodyStream(), new HttpStreamResponseHandlerNativeAdapter(streamHandler));
+        HttpStream stream = httpClientConnectionMakeRequest(getNativeHandle(),
+                request.marshalForJni(),
+                request.getBodyStream(),
+                new HttpStreamResponseHandlerNativeAdapter(streamHandler));
 
         return stream;
     }
 
     /**
-     * Determines whether a resource releases its dependencies at the same time the
-     * native handle is released or if it waits. Resources that wait are responsible
-     * for calling releaseReferences() manually.
+     * Determines whether a resource releases its dependencies at the same time the native handle is released or if it waits.
+     * Resources that wait are responsible for calling releaseReferences() manually.
      */
     @Override
-    protected boolean canReleaseReferencesImmediately() {
-        return true;
-    }
+    protected boolean canReleaseReferencesImmediately() { return true; }
 
     /**
-     * Releases this HttpClientConnection back into the Connection Pool, and allows
-     * another Request to acquire this connection.
+     * Releases this HttpClientConnection back into the Connection Pool, and allows another Request to acquire this connection.
      */
     @Override
     protected void releaseNativeHandle() {
-        if (!isNull()) {
+        if (!isNull()){
             httpClientConnectionReleaseManaged(getNativeHandle());
         }
     }
 
     /**
-     * Shuts down the underlying http connection. Even if this function is called,
-     * you still need to properly close the connection as well in order to release
-     * the native resources.
+     * Shuts down the underlying http connection.  Even if this function is called, you still need to properly close
+     * the connection as well in order to release the native resources.
      */
     public void shutdown() {
         httpClientConnectionShutdown(getNativeHandle());
@@ -87,8 +83,7 @@ public class HttpClientConnection extends CrtResource {
     };
 
     /** Called from Native when a new connection is acquired **/
-    private static void onConnectionAcquired(CompletableFuture<HttpClientConnection> acquireFuture,
-            long nativeConnectionBinding, int errorCode) {
+    private static void onConnectionAcquired(CompletableFuture<HttpClientConnection> acquireFuture, long nativeConnectionBinding, int errorCode) {
         if (errorCode != CRT.AWS_CRT_SUCCESS) {
             acquireFuture.completeExceptionally(new HttpException(errorCode));
             return;
@@ -106,13 +101,13 @@ public class HttpClientConnection extends CrtResource {
     /*******************************************************************************
      * Native methods
      ******************************************************************************/
-    private static native HttpStream httpClientConnectionMakeRequest(long connectionBinding, byte[] marshalledRequest,
-            HttpRequestBodyStream bodyStream, HttpStreamResponseHandlerNativeAdapter responseHandler)
-            throws CrtRuntimeException;
+    private static native HttpStream httpClientConnectionMakeRequest(long connectionBinding,
+                                                                     byte[] marshalledRequest,
+                                                                     HttpRequestBodyStream bodyStream,
+                                                                     HttpStreamResponseHandlerNativeAdapter responseHandler) throws CrtRuntimeException;
 
     private static native void httpClientConnectionShutdown(long connectionBinding) throws CrtRuntimeException;
 
     private static native void httpClientConnectionReleaseManaged(long connectionBinding) throws CrtRuntimeException;
-
     private static native short httpClientConnectionGetVersion(long connectionBinding) throws CrtRuntimeException;
 }

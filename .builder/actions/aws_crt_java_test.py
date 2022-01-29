030a80dc62be3f1982b3d1303842330d8a75d7d2
Dengke Tang <dengket@amazon.com>
2022-01-28 23:21:35 -0800

    run 10 in a row

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -1,4 +1,5 @@
 
+from genericpath import exists
 import Builder
 import os
 
@@ -10,11 +11,13 @@ class AWSCrtJavaTest(Builder.Action):
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
         actions = []
 
-        if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
-            -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
-            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
-            # Failed
-            actions.append("exit 1")
-        os.system("cat log.txt")
+        for i in range(10):
+            if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
+                -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
+                -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
+                # Failed
+                actions.append("exit 1")
+                os.system("cat log.txt")
+                exists
 
         return Builder.Script(actions, name='aws-crt-java-test')

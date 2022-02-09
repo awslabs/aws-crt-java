8988ad421875446b3b26e84efc1c9238a3f87ac3
Dengke Tang <dengket@amazon.com>
2022-02-09 11:07:40 -0800

    update submodules and remove the repeat

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -1,5 +1,3 @@
-
-from genericpath import exists
 import Builder
 import os
 
@@ -11,13 +9,11 @@ class AWSCrtJavaTest(Builder.Action):
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
         actions = []
 
-        for i in range(10):
-            if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
-                -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
-                -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
-                # Failed
-                actions.append("exit 1")
-                os.system("cat log.txt")
-                exists
+        if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
+            -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
+            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
+            # Failed
+            actions.append("exit 1")
+        os.system("cat log.txt")
 
         return Builder.Script(actions, name='aws-crt-java-test')

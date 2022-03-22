da14bcf77bbe421ba4e2391993c77101a5b67174
Vera Xia <zhvxia@amazon.com>
2022-03-22 16:30:50 -0700

    print log for every test to find the leak

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -1,3 +1,4 @@
+
 import Builder
 import os
 
@@ -11,7 +12,8 @@ class AWSCrtJavaTest(Builder.Action):
 
         if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
             -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
-            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
+            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true \
+            -Daws.crt.aws_trace_log_per_test"):
             # Failed
             actions.append("exit 1")
         os.system("cat log.txt")

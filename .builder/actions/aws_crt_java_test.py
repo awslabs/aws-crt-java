a4bbc0bddf80697f965f2cba026c4d988b8aac1f
Vera Xia <zhvxia@amazon.com>
2022-03-28 14:00:29 -0700

    add memory tracing

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -12,9 +12,11 @@ class AWSCrtJavaTest(Builder.Action):
 
         if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
             -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
-            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true"):
+            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true -Daws.crt.log.level=Trace \
+            -Daws.crt.log.destination=File -Daws.crt.log.filename=trace.txt"):
             # Failed
             actions.append("exit 1")
         os.system("cat log.txt")
+        os.system("cat trace.txt")
 
         return Builder.Script(actions, name='aws-crt-java-test')

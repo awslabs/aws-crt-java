0ba3bf378b92d975285b2aa919ed06463823b3c6
Noah Beard <ncbeard@amazon.com>
2022-04-01 13:59:40 -0400

    bump to trigger CI again

--- src/native/crt.c
+++ src/native/crt.c
@@ -268,6 +268,7 @@ static void s_jni_atexit_strict(void) {
          * so leave the allocators in place to avoid this
          */
         if (aws_small_block_allocator_bytes_active(s_allocator)) {
+            // Minor bump to trigger CI again...
             fprintf(stdout, "\n\n\t\tBYTES ACTIVE - SOMETHING IS LEAKING!!!\n\n");
             return;
         }

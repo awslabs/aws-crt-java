c04923df59a573c42886221677e416e0722f8423
Noah Beard <ncbeard@amazon.com>
2022-04-05 16:07:49 -0400

    Fix leaking bytes causing CI to fail

--- src/test/java/com/amazonaws/s3/S3NativeClientTest.java
+++ src/test/java/com/amazonaws/s3/S3NativeClientTest.java
@@ -294,7 +294,7 @@ public class S3NativeClientTest extends AwsClientTestFixture {
                 futures.add(nativeClient.putObject(PutObjectRequest.builder().bucket(BUCKET).key(PUT_OBJECT_KEY)
                         .contentLength(contentLength).build(), buffer -> {
                             while (buffer.hasRemaining()) {
-                                buffer.put((byte) 42);
+                                buffer.put((byte) 65);
                                 ++lengthWritten[0];
                             }
 

35967e00aa0cd5afd1bed204c81c8a9180f93706
Noah Beard <ncbeard@amazon.com>
2022-04-06 12:00:18 -0400

    Bump to rerun CI

--- src/test/java/com/amazonaws/s3/S3NativeClientTest.java
+++ src/test/java/com/amazonaws/s3/S3NativeClientTest.java
@@ -303,7 +303,7 @@ public class S3NativeClientTest extends AwsClientTestFixture {
                 futures.add(nativeClient.putObject(PutObjectRequest.builder().bucket(BUCKET).key(PUT_OBJECT_KEY)
                         .contentLength(contentLength).build(), buffer -> {
                             while (buffer.hasRemaining()) {
-                                buffer.put((byte) 65); // A single byte! This is likely where the allocation issue is occuring!
+                                buffer.put((byte) 65); // A single byte! This is likely where the allocation issue is occuring! BUMP 2
                                 ++lengthWritten[0];
                             }
 

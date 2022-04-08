5437a9b0e742327c7e63ae1e8b2bc4e50259e800
Noah Beard <ncbeard@amazon.com>
2022-04-08 16:18:04 -0400

    Try logging but without a separate log for each test to see if that changes anything

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -12,7 +12,7 @@ class AWSCrtJavaTest(Builder.Action):
 
         if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
             -DrerunFailingTestsCount=5 -DskipAfterFailureCount=1 \
-            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true -Daws.crt.aws_trace_log_per_test"):
+            -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true -Daws.crt.log.level=Trace"):
             # Failed
             actions.append("exit 1")
         os.system("cat log.txt")

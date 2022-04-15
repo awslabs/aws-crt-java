3469efc1c6284d4eb9d837458d9bc1f6371ffbe2
Michael Graeb <graebm@amazon.com>
2022-04-15 11:58:09 -0700

    add automation

--- make-docs.sh
+++ make-docs.sh
@@ -2,6 +2,14 @@
 
 pushd $(dirname $0) > /dev/null
 
+# clean
+rm -rf docs/
+
+# build
 javadoc @javadoc.options
+
+# The existence of this file tells GitHub Pages to just host the HTML as-is
+# https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
+touch docs/.nojekyll
 
 popd > /dev/null

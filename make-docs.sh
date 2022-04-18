1ecf6894e3b58539d7f7d10202fda0c210076d4a
Michael Graeb <graebm@amazon.com>
2022-04-18 09:15:23 -0700

    .nojekyll file isn't actually necessary

--- make-docs.sh
+++ make-docs.sh
@@ -10,8 +10,4 @@ rm -rf docs/
 # build
 javadoc @javadoc.options
 
-# The existence of this file tells GitHub Pages to just host the HTML as-is
-# https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
-touch docs/.nojekyll
-
 popd > /dev/null

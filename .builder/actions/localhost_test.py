78aebd81890b4511593afa98411a40b91b3ba3e5
Dengke Tang <dengket@amazon.com>
2022-05-20 15:38:18 -0700

    forgot to commit action

--- .builder/actions/localhost_test.py
+++ .builder/actions/localhost_test.py
@@ -7,6 +7,19 @@ class LocalhostTest(Builder.Action):
 
     def run(self, env):
         # tests must run with leak detection turned on
-        actions = ["echo thisShouldBeLocalTest"]
+        env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
+        actions = []
+        if os.system("mvn -B test -DredirectTestOutputToFile=true -DforkCount=0 \
+            -DrerunFailingTestsCount=5 \
+            -Daws.crt.memory.tracing=2 \
+            -Daws.crt.debugnative=true \
+            -Daws.crt.localhost=true"):
+            # Failed
+            actions.append("exit 1")
+        os.system("cat log.txt")
+        python = sys.executable
+        actions.append(
+            [python, 'crt/aws-c-http/integration-testing/http_client_test.py',
+                python, 'integration-testing/java_elasticurl_runner.py'])
 
-        return Builder.Script(actions, name='localhost-test')
+        return Builder.Script(actions, name='aws-crt-java-test')

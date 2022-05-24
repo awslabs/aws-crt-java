6517d5104573392c3a3cfea9a153f7b2da80ed85
Dengke Tang <dengket@amazon.com>
2022-05-23 17:22:24 -0700

    get rid of sutff not needed

--- .builder/actions/localhost_test.py
+++ .builder/actions/localhost_test.py
@@ -6,7 +6,6 @@ import os
 class LocalhostTest(Builder.Action):
 
     def run(self, env):
-        # tests must run with leak detection turned on
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
         actions = []
         if os.system("mvn -Dtest=Http2ClientLocalHostTest test -DredirectTestOutputToFile=true -DforkCount=0 \
@@ -16,9 +15,5 @@ class LocalhostTest(Builder.Action):
             -Daws.crt.localhost=true"):
             # Failed
             actions.append("exit 1")
-        python = sys.executable
-        actions.append(
-            [python, 'crt/aws-c-http/integration-testing/http_client_test.py',
-                python, 'integration-testing/java_elasticurl_runner.py'])
 
         return Builder.Script(actions, name='aws-crt-java-test')

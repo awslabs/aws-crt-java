662e0a5264efa2ff31dd90304d4c3cd4916b7f62
Dengke Tang <dengket@amazon.com>
2022-07-15 16:06:52 -0700

    hmmm

--- .builder/actions/localhost_test.py
+++ .builder/actions/localhost_test.py
@@ -7,10 +7,14 @@ class LocalhostTest(Builder.Action):
 
     def run(self, env):
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
-        actions = ["mvn -Dtest=Http2ClientLocalHostTest test -DredirectTestOutputToFile=true -DforkCount=0 \
+        actions = []
+        if os.system("mvn -Dtest=Http2ClientLocalHostTest test -DredirectTestOutputToFile=true -DforkCount=0 \
             -DrerunFailingTestsCount=5 \
             -Daws.crt.memory.tracing=2 \
             -Daws.crt.debugnative=true \
             -Daws.crt.log.level=Error \
-            -Daws.crt.localhost=true"]
+            -Daws.crt.localhost=true"):
+            # Failed
+            actions.append("exit 1")
+
         return Builder.Script(actions, name='aws-crt-java-test')

bc2c326162d405312a48265cb97c1b7c35803bf0
Dengke Tang <dengket@amazon.com>
2022-07-20 09:43:23 -0700

    why it hangs?

--- .builder/actions/localhost_test.py
+++ .builder/actions/localhost_test.py
@@ -8,11 +8,11 @@ class LocalhostTest(Builder.Action):
     def run(self, env):
         env.shell.setenv('AWS_CRT_MEMORY_TRACING', '2')
         actions = []
-        if os.system("mvn -Dtest=Http2ClientLocalHostTest test -DredirectTestOutputToFile=true -DforkCount=0 \
+        if os.system("mvn -Dtest=Http2ClientLocalHostTest#testParallelRequestsStress test -DredirectTestOutputToFile=true -DforkCount=0 \
             -DrerunFailingTestsCount=5 \
             -Daws.crt.memory.tracing=2 \
             -Daws.crt.debugnative=true \
-            -Daws.crt.log.level=Error \
+            -Daws.crt.log.level=Debug \
             -Daws.crt.localhost=true"):
             # Failed
             actions.append("exit 1")

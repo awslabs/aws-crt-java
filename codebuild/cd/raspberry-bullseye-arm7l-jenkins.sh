82c03a4463bf7dad1ad0822ebbf697d401c99190
Zhihui Xia <zhvxia@amazon.com>
2022-10-11 10:20:03 -0700

    set image from test to latest

--- codebuild/cd/raspberry-bullseye-arm7l-jenkins.sh
+++ codebuild/cd/raspberry-bullseye-arm7l-jenkins.sh
@@ -11,7 +11,7 @@ echo "Using builder version ${BUILDER_VERSION}"
 
 aws s3 cp s3://aws-crt-builder/releases/${BUILDER_VERSION}/builder.pyz ./builder
 
-DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/raspbian-bullseye-arm7l:light
+DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/raspbian-bullseye-arm7l:latest
 
 $(aws --region us-east-1 ecr get-login --no-include-email)
 

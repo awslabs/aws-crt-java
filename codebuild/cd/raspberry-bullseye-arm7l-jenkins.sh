09058e6b6a9384fa4725fda9a5c74643189116f6
Zhihui Xia <zhvxia@amazon.com>
2022-10-19 10:22:53 -0700

    java cd script

--- codebuild/cd/raspberry-bullseye-arm7l-jenkins.sh
+++ codebuild/cd/raspberry-bullseye-arm7l-jenkins.sh
@@ -2,7 +2,6 @@
 #run build-wheels script in manylinux2014 docker image
 set -ex
 
-# Set LIB_PATH as cross compiles as raspbery pi would be handled as linux-armv7
 LIB_PATH=target/cmake-build/aws-crt-java/lib
 
 # Pry the builder version this CRT is using out of ci.yml
@@ -11,7 +10,7 @@ echo "Using builder version ${BUILDER_VERSION}"
 
 aws s3 cp s3://aws-crt-builder/releases/${BUILDER_VERSION}/builder.pyz ./builder
 
-DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/raspbian-bullseye-arm7l:latest
+DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/raspbian-bullseye-armv7l:test
 
 $(aws --region us-east-1 ecr get-login --no-include-email)
 
@@ -27,6 +26,7 @@ docker run --rm \
     --platform linux/arm/v7 \
     $DOCKER_IMAGE \
     codebuild/cd/raspberry-bullseye-arm7l.sh
+
 
 # Upload the lib to S3
 GIT_TAG=$(git describe --tags)
--- codebuild/cd/raspberry-bullseye-arm7l.sh
+++ codebuild/cd/raspberry-bullseye-arm7l.sh
@@ -5,4 +5,4 @@ set -ex
 cd `dirname $0`/../..
 
 chmod a+x builder
-./builder build -p aws-crt-java run_tests=false
+./builder build -p aws-crt-java run_tests=false --target=linux-armv7

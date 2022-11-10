8023fcb6825ca2e64079c97ef268ef665e5d7290
Bret Ambrose <bretambrose@gmail.com>
2022-11-09 16:28:10 -0800

    Strict Pkcs11 for tls context options test too

--- src/test/java/software/amazon/awssdk/crt/test/TlsContextOptionsTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/TlsContextOptionsTest.java
@@ -224,7 +224,7 @@ public class TlsContextOptionsTest extends CrtTestFixture {
         Assume.assumeTrue(System.getProperty("NETWORK_TESTS_DISABLED") == null);
         Pkcs11LibTest.assumeEnvironmentSetUpForPkcs11Tests();
 
-        try (Pkcs11Lib pkcs11Lib = new Pkcs11Lib(Pkcs11LibTest.TEST_PKCS11_LIB);
+        try (Pkcs11Lib pkcs11Lib = new Pkcs11Lib(Pkcs11LibTest.TEST_PKCS11_LIB, Pkcs11Lib.InitializeFinalizeBehavior.STRICT);
                 TlsContextPkcs11Options pkcs11Options = new TlsContextPkcs11Options(pkcs11Lib)
                         .withUserPin(Pkcs11LibTest.TEST_PKCS11_PIN)
                         .withTokenLabel(Pkcs11LibTest.TEST_PKCS11_TOKEN_LABEL)

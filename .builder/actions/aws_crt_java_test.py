5390cb9981d176f21e7d4c4960627e72cd91c599
xiazhvera <zhvxia@amazon.com>
2022-11-10 10:56:11 -0800

    Added unit test to setup Mqtt Connect through EC-based certificate and key (#545)
    * adding ecc key unit test
    
    * revert test api changes, keep the original api unchanged
    
    * fix ecc private key variable name
    
    * update README with ecc related test parameters
    
    * update c-io
    
    * update c-common
    
    * update java test arguments
    
    * rename parameter to make it clear
    
    * rename test context

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -22,11 +22,14 @@ class AWSCrtJavaTest(Builder.Action):
 
         endpoint = env.shell.get_secret("unit-test/endpoint")
 
-        with self._write_secret_to_temp_file(env, "unit-test/rootca") as root_ca_file, self._write_secret_to_temp_file(env, "unit-test/certificate") as cert_file, self._write_secret_to_temp_file(env, "unit-test/privatekey") as key_file:
+        with self._write_secret_to_temp_file(env, "unit-test/rootca") as root_ca_file, self._write_secret_to_temp_file(env, "unit-test/certificate") as cert_file, \
+        self._write_secret_to_temp_file(env, "unit-test/privatekey") as key_file, self._write_secret_to_temp_file(env, "ecc-test/certificate") as ecc_cert_file, \
+        self._write_secret_to_temp_file(env, "ecc-test/privatekey") as ecc_key_file:
 
             test_command = "mvn -P continuous-integration -B test -DredirectTestOutputToFile=true -DreuseForks=false " \
                 "-DrerunFailingTestsCount=5 -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true -Daws.crt.ci=true " \
-                "-Dendpoint={} -Dcertificate={} -Dprivatekey={} -Drootca={}".format(endpoint, cert_file.name, key_file.name, root_ca_file.name)
+                "-Dendpoint={} -Dcertificate={} -Dprivatekey={} -Drootca={} -Decc_certificate={} -Decc_privatekey={}".format(endpoint, \
+                cert_file.name, key_file.name, root_ca_file.name, ecc_cert_file.name, ecc_key_file.name)
 
             all_test_result = os.system(test_command)
 
--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -13,6 +13,8 @@ git submodule update --init
 curl https://www.amazontrust.com/repository/AmazonRootCA1.pem --output /tmp/AmazonRootCA1.pem
 cert=$(aws secretsmanager get-secret-value --secret-id "unit-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/certificate.pem
 key=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/privatekey.pem
+ecc_cert=$(aws secretsmanager get-secret-value --secret-id "ecc-test/certificate" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$cert" > /tmp/ecc_certificate.pem
+ecc_privatekey=$(aws secretsmanager get-secret-value --secret-id "ecc-test/privatekey" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key" > /tmp/ecc_privatekey.pem
 key_p8=$(aws secretsmanager get-secret-value --secret-id "unit-test/privatekey-p8" --query "SecretString" | cut -f2 -d":" | cut -f2 -d\") && echo -e "$key_p8" > /tmp/privatekey_p8.pem
 ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "unit-test/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 
@@ -26,6 +28,8 @@ mvn -B test $* \
     -Dendpoint=$ENDPOINT \
     -Dcertificate=/tmp/certificate.pem \
     -Dprivatekey=/tmp/privatekey.pem \
+    -Decc_certificate=/tmp/ecc_certificate.pem \
+    -Decc_privatekey=/tmp/ecc_privatekey.pem \
     -Drootca=/tmp/AmazonRootCA1.pem \
     -Dprivatekey_p8=/tmp/privatekey_p8.pem \
     -Daws.crt.debugnative=true \

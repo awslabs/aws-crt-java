fd1233fbcbc7947249709b6b2504924989b3da57
Dengke Tang <dengket@amazon.com>
2022-11-09 16:35:25 -0800

    in case of failure, still print out the trace log

--- .builder/actions/localhost_test.py
+++ .builder/actions/localhost_test.py
@@ -11,8 +11,12 @@ class LocalhostTest(Builder.Action):
         if os.system("mvn -Dtest=Http2ClientLocalHostTest test -DredirectTestOutputToFile=true -DforkCount=0 \
             -Daws.crt.memory.tracing=2 \
             -Daws.crt.debugnative=true \
+            -Daws.crt.log.level=Trace \
+            -Daws.crt.log.destination=File \
+            -Daws.crt.log.filename=trace.log \
             -Daws.crt.localhost=true"):
             # Failed
+            os.system("cat trace.log")
             actions.append("exit 1")
 
         return Builder.Script(actions, name='aws-crt-java-test')

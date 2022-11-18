b9c5da4742fb064b1a95e52d279266be6173cf55
Zhihui Xia <zhvxia@amazon.com>
2022-11-18 09:27:54 -0800

    fix arch string match

--- codebuild/macos_compatibility_check.py
+++ codebuild/macos_compatibility_check.py
@@ -7,16 +7,20 @@ if sys.platform != 'darwin':
     print("WARNING: Not running on macos. Skip the compatibility validation.")
     sys.exit(True)
 
+# Default target macos version setup in pom.xml > ${cmake.min_osx_version}
 supported_version = "10.9"
 arch = "x86_64"
-machine_id = os.uname()[4]
-m = re.match(r'^(aarch64)', machine_id.strip())
-if m == 'aarch64':
-    # The oldest version we can target on arm64 is 11
-    supported_version = "11.0"
-    arch = "armv8"
 
-if os.system("otool -l target/classes/osx/{}/libaws-crt-jni.dylib | grep -E minos | cut -f2 -ds".format(arch), capture_output=True) != supported_version:
+machine_id = os.uname().machine
+m = re.match(r'^(aarch64|armv[6-8]|arm64)', machine_id.strip())
+if m:
+    # The oldest version we can target on arm64 is 11.0
+    supported_version = "11.0"
+    arch = m.group(1)
+    if arch == 'aarch64' or arch == 'arm64':
+        arch = "armv8"
+
+if os.system("otool -l target/classes/osx/{}/libaws-crt-jni.dylib | grep -E minos | cut -f2 -ds".format(arch)) != supported_version:
     # Failed
     sys.exit(False)
 

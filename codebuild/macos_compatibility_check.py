e700032115e58436fb034f71e4f0993efcc1ba83
Zhihui Xia <zhvxia@amazon.com>
2022-11-21 13:26:17 -0800

    remove spaces in version string

--- codebuild/macos_compatibility_check.py
+++ codebuild/macos_compatibility_check.py
@@ -24,7 +24,7 @@ def main():
     #   version 10.9
     #       sdk 12.1
     # Load command 9
-    otool_cmd = "otool -l target/cmake-build/lib/osx/{}/libaws-crt-jni.dylib | grep -A3 \'LC_VERSION_MIN_MACOSX\' | grep -E version | tr -s ' ' | cut -f3 -d' '".format(arch)
+    otool_cmd = "otool -l target/cmake-build/lib/osx/{}/libaws-crt-jni.dylib | grep -A3 \'LC_VERSION_MIN_MACOSX\' | grep -E version | tr -s ' ' | cut -f3 -d' '| tr -d ' ".format(arch)
 
     if len(sys.argv) > 1:
         # Parsing the macos archtecture
@@ -38,14 +38,14 @@ def main():
         arch = "armv8"
         # The oldest version we can target on arm64 is 11.0
         supported_version = "11.0"
-        otool_cmd = "otool -l target/cmake-build/lib/osx/{}/libaws-crt-jni.dylib | grep -E minos | tr -s ' ' | cut -f3 -d' '".format(arch)
+        otool_cmd = "otool -l target/cmake-build/lib/osx/{}/libaws-crt-jni.dylib | grep -E minos | tr -s ' ' | cut -f3 -d' ' | tr -d ' '| tr -d ' '".format(arch)
 
     print("Start to validate the build binary for MacOS with architecture {}, expected min os version: {}".format(arch,supported_version))
-
     result = subprocess.check_output(otool_cmd, shell=True).decode("utf-8")
+
     if result != supported_version:
         # Failed
-        print("Failed the compatibility validation on MacOS architecture {}, expected {} and built {}".format(arch, supported_version, result))
+        print("Failed the compatibility validation on MacOS architecture {}, expected '{}' and built '{}'".format(arch, supported_version, result))
         sys.exit(1)
 
     print("Pass the compatibility validation on MacOS architecture {} with min supported os version '{}'".format(arch,result))

1b7def52e451528f4763f707f6bf5a406c655f1d
Noah Beard <ncbeard@amazon.com>
2022-11-25 11:22:18 -0500

    Install CMake on Windows Codebuild

--- codebuild/mqtt5-tests-windows.bat
+++ codebuild/mqtt5-tests-windows.bat
@@ -9,6 +9,8 @@ pushd %~dp0\..\
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 :: this will also install jdk8
 choco install adoptopenjdk8 maven -y
+:: Install Cmake
+choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System' -y
 call RefreshEnv.cmd
 echo JAVA_HOME=%JAVA_HOME%
 

8e80bb2dacf3bf8cfd6d7b3103face02b2c15e2c
Noah Beard <ncbeard@amazon.com>
2022-11-25 11:40:58 -0500

    CI bump

--- codebuild/mqtt5-tests-windows.bat
+++ codebuild/mqtt5-tests-windows.bat
@@ -19,3 +19,4 @@ mvn install -DskipTests
 call ./utils/mqtt5_test_setup.sh s3://aws-crt-test-stuff/CodeBuildIotProdMQTT5EnvironmentVariables.txt us-east-1
 mvn test -Dtest=Mqtt5ClientTest -DfailIfNoTests=false
 call ./utils/mqtt5_test_setup.sh s3://aws-crt-test-stuff/CodeBuildIotProdMQTT5EnvironmentVariables.txt cleanup
+

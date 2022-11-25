1d2537e3f06f13ebf0da939df2d26e2cd67c72e4
Noah Beard <ncbeard@amazon.com>
2022-11-25 11:09:26 -0500

    Add Windows Codebuild for MQTT5 tests

---
+++ codebuild/mqtt5-tests-windows.bat
@@ -0,0 +1,19 @@
+
+@echo off
+
+@setlocal enableextensions enabledelayedexpansion
+
+pushd %~dp0\..\
+
+ :: install chocolatey
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+:: this will also install jdk8
+choco install adoptopenjdk8 maven -y
+call RefreshEnv.cmd
+echo JAVA_HOME=%JAVA_HOME%
+
+cd %CODEBUILD_SRC_DIR%
+mvn install -DskipTests
+call ./utils/mqtt5_test_setup.sh s3://aws-crt-test-stuff/CodeBuildIotProdMQTT5EnvironmentVariables.txt us-east-1
+mvn test -Dtest=Mqtt5ClientTest -DfailIfNoTests=false
+call ./utils/mqtt5_test_setup.sh s3://aws-crt-test-stuff/CodeBuildIotProdMQTT5EnvironmentVariables.txt cleanup

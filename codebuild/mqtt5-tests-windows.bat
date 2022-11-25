89676724ebbd7b7971728c85339f2c7528d53c27
Noah Beard <ncbeard@amazon.com>
2022-11-25 15:53:54 -0500

    Run the tests too

--- codebuild/mqtt5-tests-windows.bat
+++ codebuild/mqtt5-tests-windows.bat
@@ -16,8 +16,8 @@ call RefreshEnv.cmd
 echo JAVA_HOME=%JAVA_HOME%
 
 cd %CODEBUILD_SRC_DIR%
-mvn install -DskipTests
+call mvn install -DskipTests
 call ./utils/mqtt5_test_setup.sh s3://aws-crt-test-stuff/CodeBuildIotProdMQTT5EnvironmentVariables.txt us-east-1
-mvn test -Dtest=Mqtt5ClientTest -DfailIfNoTests=false
+call mvn test -Dtest=Mqtt5ClientTest -DfailIfNoTests=false
 call ./utils/mqtt5_test_setup.sh s3://aws-crt-test-stuff/CodeBuildIotProdMQTT5EnvironmentVariables.txt cleanup
 

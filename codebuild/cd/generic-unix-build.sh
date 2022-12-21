79d057cb46098ae8861f3d4665033dac2c434074
Dengke Tang <dengket@amazon.com>
2022-12-21 10:26:32 -0800

    fine, still let build to do cmake only

--- codebuild/cd/generic-unix-build.sh
+++ codebuild/cd/generic-unix-build.sh
@@ -31,6 +31,10 @@ GIT_TAG=$(git describe --tags)
 # use a fix deploy version for platform specific jar
 mvn versions:set -DnewVersion=deploy
 ./builder build -p aws-crt-java --target=$AWS_CRT_TARGET run_tests=false
+# Builder corss compile the shared lib to `target/cmake-build/aws-crt-java/`, move it to th expected path for mvn to generate jar.
+mv target/cmake-build/aws-crt-java/* target/cmake-build/
 
-aws s3 cp --recursive --exclude "*.a" target/cmake-build/lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 mvn -B install -DskipTests -P$AWS_CRT_TARGET -Dshared-lib.skip=true
+
+aws s3 cp --recursive --include "*.so" target/cmake-build/lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib
 aws s3 cp target/ s3://aws-crt-java-pipeline/${GIT_TAG}/jar/ --recursive --exclude "*" --include "aws-crt*.jar"

ccd7be429e1a142a97f1e610253f4835612794a3
Dengke Tang <dengket@amazon.com>
2023-01-03 16:48:11 -0800

    check for file exist

--- .builder/actions/aws_crt_java_test.py
+++ .builder/actions/aws_crt_java_test.py
@@ -4,6 +4,9 @@ import sys
 import os
 import tempfile
 
+LOG_PATH = "log.txt"
+
+
 class AWSCrtJavaTest(Builder.Action):
 
     def _write_secret_to_temp_file(self, env, secret_name):
@@ -23,13 +26,13 @@ class AWSCrtJavaTest(Builder.Action):
         endpoint = env.shell.get_secret("unit-test/endpoint")
 
         with self._write_secret_to_temp_file(env, "unit-test/rootca") as root_ca_file, self._write_secret_to_temp_file(env, "unit-test/certificate") as cert_file, \
-        self._write_secret_to_temp_file(env, "unit-test/privatekey") as key_file, self._write_secret_to_temp_file(env, "ecc-test/certificate") as ecc_cert_file, \
-        self._write_secret_to_temp_file(env, "ecc-test/privatekey") as ecc_key_file:
+                self._write_secret_to_temp_file(env, "unit-test/privatekey") as key_file, self._write_secret_to_temp_file(env, "ecc-test/certificate") as ecc_cert_file, \
+                self._write_secret_to_temp_file(env, "ecc-test/privatekey") as ecc_key_file:
 
             test_command = "mvn -P continuous-integration -B test -DredirectTestOutputToFile=true -DreuseForks=false " \
                 "-DrerunFailingTestsCount=5 -Daws.crt.memory.tracing=2 -Daws.crt.debugnative=true -Daws.crt.ci=true " \
-                "-Dendpoint={} -Dcertificate={} -Dprivatekey={} -Drootca={} -Decc_certificate={} -Decc_privatekey={}".format(endpoint, \
-                cert_file.name, key_file.name, root_ca_file.name, ecc_cert_file.name, ecc_key_file.name)
+                "-Dendpoint={} -Dcertificate={} -Dprivatekey={} -Drootca={} -Decc_certificate={} -Decc_privatekey={}".format(endpoint,
+                                                                                                                             cert_file.name, key_file.name, root_ca_file.name, ecc_cert_file.name, ecc_key_file.name)
 
             all_test_result = os.system(test_command)
 
@@ -40,7 +43,8 @@ class AWSCrtJavaTest(Builder.Action):
         if shutdown_test_result or all_test_result:
             # Failed
             actions.append("exit 1")
-        os.system("cat log.txt")
+        if os.path.isfile(LOG_PATH):
+            os.system(f"cat {LOG_PATH}")
         python = sys.executable
         actions.append(
             [python, 'crt/aws-c-http/integration-testing/http_client_test.py',

5cd69c601b25832b9e52f3e45c4c0fac3968a81a
Dengke Tang <dengket@amazon.com>
2023-01-04 11:20:28 -0800

    remove smithy-crt as the codegen tool that not used

--- smithy-crt/src/main/kotlin/software/amazon/smithy/crt/codegen/SourceGenerator.kt
+++
@@ -1,7 +0,0 @@
-package software.amazon.smithy.crt.codegen
-
-import java.nio.file.Path
-import java.util.function.BiConsumer
-import java.util.function.Consumer
-
-interface SourceGenerator : BiConsumer<Path, Consumer<Path>> {}

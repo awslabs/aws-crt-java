82293dad3137f6d94bb0a3f912f4b77028494917
Bret Ambrose <bretambrose@gmail.com>
2023-01-06 10:37:24 -0800

    First attempt at classifier artifact

---
+++ .builder/actions/build-classifier.py
@@ -0,0 +1,19 @@
+
+import Builder
+import sys
+import os
+import tempfile
+from builder.core.host import current_host
+
+class BuildClassifier(Builder.Action):
+
+
+    def run(self, env):
+
+        crt_classifier = os.getenv("CRT_CLASSIFIER")
+        if crt_classifier is not None and os.system(f"mvn -B install -DskipTests -Dshared-lib.skip=true -Dcrt.classifier={crt_classifier}"):
+
+            # Failed
+            actions.append("exit 1")
+
+        return Builder.Script(actions, name='build-classifier')
--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -20,21 +20,16 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker volume create --name=artifacts
 export VOLUME_PATH=$(docker volume inspect artifacts | jq -r '.[0].Mountpoint')
 export CRT_CLASSIFIER=${CLASSIFIER}
-docker run --mount type=volume,src=artifacts,dst=/root/aws-crt-java --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --env CRT_CLASSIFIER $DOCKER_IMAGE --version=${BUILDER_VERSION} $@
+export BRANCH_TAG=$(git describe --tags)
+docker run --mount type=volume,src=artifacts,dst=/root/aws-crt-java --env AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --env CRT_CLASSIFIER $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --branch ${BRANCH_TAG} run_tests=false
 mkdir -p /tmp/artifacts
 cp -a ${VOLUME_PATH}/target /tmp/artifacts
 docker container prune -f
 docker volume rm artifacts
 
-#./builder build -p aws-crt-java --target=$AWS_CRT_TARGET run_tests=false
-# Builder corss-compiles the shared lib to `target/cmake-build/aws-crt-java/`, move it to the expected path for mvn to generate the jar.
-#mv target/cmake-build/aws-crt-java/* target/cmake-build/
-
-#JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 mvn -B install -DskipTests -Dshared-lib.skip=true -Dcrt.classifier=$CLASSIFIER
-
-# Upload the lib to S3
+# Upload the artifacts to S3
 #GIT_TAG=$(git describe --tags)
 export GIT_TAG="musl-testing"
 
 aws s3 cp --recursive --include "*.so" /tmp/artifacts/target/cmake-build/lib s3://aws-crt-java-pipeline/${GIT_TAG}/lib
-#aws s3 cp /tmp/artifacts/target/ s3://aws-crt-java-pipeline/${GIT_TAG}/jar/ --recursive --exclude "*" --include "aws-crt*.jar"
+aws s3 cp /tmp/artifacts/target/ s3://aws-crt-java-pipeline/${GIT_TAG}/jar/ --recursive --exclude "*" --include "aws-crt*.jar"

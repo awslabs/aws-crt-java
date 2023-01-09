35d4215d9e61d80c86a577d34d801f75187a0558
Bret Ambrose <bretambrose@gmail.com>
2023-01-09 10:39:48 -0800

    Undefined value

--- .builder/actions/build-classifier.py
+++ .builder/actions/build-classifier.py
@@ -10,6 +10,8 @@ class BuildClassifier(Builder.Action):
 
     def run(self, env):
 
+        actions = []
+        
         crt_classifier = os.getenv("CRT_CLASSIFIER")
         if crt_classifier is not None and os.system(f"mvn -B install -DskipTests -Dshared-lib.skip=true -Dcrt.classifier={crt_classifier}"):
 

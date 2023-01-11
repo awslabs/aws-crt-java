0143b978d20dbb0a9ea75e660c78e4f4961b158a
Noah Beard <ncbeard@amazon.com>
2023-01-11 16:07:22 -0500

    Minor improvements and code clean up, add back running all tests (minus HTTP proxy ones) again

--- codebuild/common-linux-setup-mosquitto.sh
+++ codebuild/common-linux-setup-mosquitto.sh
@@ -7,34 +7,43 @@ mkdir -p MosquittoInstall
 cd MosquittoInstall
 
 # mosquitto needs OpenSSL at version 1.1.0+, so we have to install from source
-echo "Setup mosquitto dependencies - OpenSSL"
-git clone https://github.com/openssl/openssl.git --branch OpenSSL_1_1_1s
-cd openssl
-./config --prefix=/usr/local --openssldir=/usr/local
-make
-sudo make install
+echo "Setup mosquitto dependencies - OpenSSL (may take a couple minutes - should take no more than 6 minutes)"
+# Note: 99% of the time we do not care for the output, so let's hide it to reduce console spam!
+{
+    git clone https://github.com/openssl/openssl.git --branch OpenSSL_1_1_1s
+    cd openssl
+    ./config --prefix=/usr/local --openssldir=/usr/local
+    make
+    sudo make install
+} > /dev/null
 
 # And we have to install libwebsockets from source for mosquitto too
-echo Setup mosquitto dependencies - LibWebsockets
-git clone https://github.com/warmcat/libwebsockets.git --branch v4.3.2
-cd libwebsockets
-mkdir build
-cd build
-cmake .. -DLWS_WITH_EXTERNAL_POLL=ON
-make
-sudo make install
+# Note: 99% of the time we do not care for the output, so let's hide it to reduce console spam!
+echo "Setup mosquitto dependencies - LibWebsockets (may take a couple minutes - should take no more than 6 minutes)"
+{
+    git clone https://github.com/warmcat/libwebsockets.git --branch v4.3.2
+    cd libwebsockets
+    mkdir build
+    cd build
+    cmake .. -DLWS_WITH_EXTERNAL_POLL=ON
+    make
+    sudo make install
 cd ../..
+} > /dev/null
 
 # Install mosquitto from source
-echo Setup mosquitto - Mosquitto from source
-git clone https://github.com/eclipse/mosquitto.git --branch v2.0.15
-cd mosquitto
-make install WITH_DOCS=no WITH_CJSON=no WITH_WEBSOCKETS=yes LDFLAGS="-L/usr/local/lib/  -Wl,-rpath,/usr/local/lib/"
-cd ..
-sudo ldconfig
+# Note: 99% of the time we do not care for the output, so let's hide it to reduce console spam!
+echo "Setup mosquitto - Mosquitto from source (may take a couple minutes - should take no more than 6 minutes)"
+{
+    git clone https://github.com/eclipse/mosquitto.git --branch v2.0.15
+    cd mosquitto
+    make install WITH_DOCS=no WITH_CJSON=no WITH_WEBSOCKETS=yes LDFLAGS="-L/usr/local/lib/  -Wl,-rpath,/usr/local/lib/"
+    cd ..
+    sudo ldconfig
+} > /dev/null
 
 # Get the config file and run it
-echo Setup mosquitto - configure mosquitto and start it running
+echo "Setup mosquitto - configure mosquitto and start it running"
 aws s3 cp s3://aws-crt-test-stuff/setup_mosquitto_test_env.sh ./setup_mosquitto_test_env.sh
 sudo chmod a+xr ./setup_mosquitto_test_env.sh
 ./setup_mosquitto_test_env.sh

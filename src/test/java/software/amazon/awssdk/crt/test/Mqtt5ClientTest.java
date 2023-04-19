0dcc1a1dd847217a84e56495796a20d2c12fa2fb
Noah Beard <ncbeard@amazon.com>
2023-04-19 15:53:15 -0400

    Longer ConnAck timeout?

--- src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
@@ -1455,12 +1455,12 @@ public class Mqtt5ClientTest extends CrtTestFixture {
             Mqtt5ClientOptionsBuilder builder = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builder.withLifecycleEvents(eventsOne);
             builder.withConnectOptions(connectOptions.build());
-            builder.withConnackTimeoutMs(10000l); // 10 seconds
+            builder.withConnackTimeoutMs(60000l); // 60 seconds
 
             Mqtt5ClientOptionsBuilder builderTwo = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builderTwo.withLifecycleEvents(eventsTwo);
             builderTwo.withConnectOptions(connectOptions.build());
-            builderTwo.withConnackTimeoutMs(10000l); // 10 seconds
+            builderTwo.withConnackTimeoutMs(60000l); // 60 seconds
 
             // Only needed for IoT Core
             TlsContext tlsContext = null;
@@ -1513,12 +1513,12 @@ public class Mqtt5ClientTest extends CrtTestFixture {
             Mqtt5ClientOptionsBuilder builder = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builder.withLifecycleEvents(eventsOne);
             builder.withConnectOptions(connectOptions.build());
-            builder.withConnackTimeoutMs(10000l); // 10 seconds
+            builder.withConnackTimeoutMs(60000l); // 60 seconds
 
             Mqtt5ClientOptionsBuilder builderTwo = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builderTwo.withLifecycleEvents(eventsTwo);
             builderTwo.withConnectOptions(connectOptions.build());
-            builderTwo.withConnackTimeoutMs(10000l); // 10 seconds
+            builderTwo.withConnackTimeoutMs(60000l); // 60 seconds
 
             // Only needed for IoT Core
             TlsContext tlsContext = null;

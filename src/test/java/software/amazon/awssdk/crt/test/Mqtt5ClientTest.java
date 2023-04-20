840b5584430b70a8a847b11186f574f0f98dba67
Noah Beard <ncbeard@amazon.com>
2023-04-20 09:48:23 -0400

    Set the ConnAck time to 30 seconds

--- src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
@@ -1455,12 +1455,12 @@ public class Mqtt5ClientTest extends CrtTestFixture {
             Mqtt5ClientOptionsBuilder builder = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builder.withLifecycleEvents(eventsOne);
             builder.withConnectOptions(connectOptions.build());
-            builder.withConnackTimeoutMs(20000l); // 20 seconds
+            builder.withConnackTimeoutMs(30000l); // 30 seconds
 
             Mqtt5ClientOptionsBuilder builderTwo = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builderTwo.withLifecycleEvents(eventsTwo);
             builderTwo.withConnectOptions(connectOptions.build());
-            builderTwo.withConnackTimeoutMs(20000l); // 20 seconds
+            builderTwo.withConnackTimeoutMs(30000l); // 30 seconds
 
             // Only needed for IoT Core
             TlsContext tlsContext = null;
@@ -1514,12 +1514,12 @@ public class Mqtt5ClientTest extends CrtTestFixture {
             Mqtt5ClientOptionsBuilder builder = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builder.withLifecycleEvents(eventsOne);
             builder.withConnectOptions(connectOptions.build());
-            builder.withConnackTimeoutMs(20000l); // 20 seconds
+            builder.withConnackTimeoutMs(30000l); // 30 seconds
 
             Mqtt5ClientOptionsBuilder builderTwo = new Mqtt5ClientOptionsBuilder(getMinimumDirectHost(), getMinimumDirectPort());
             builderTwo.withLifecycleEvents(eventsTwo);
             builderTwo.withConnectOptions(connectOptions.build());
-            builderTwo.withConnackTimeoutMs(20000l); // 20 seconds
+            builderTwo.withConnackTimeoutMs(30000l); // 30 seconds
 
             // Only needed for IoT Core
             TlsContext tlsContext = null;

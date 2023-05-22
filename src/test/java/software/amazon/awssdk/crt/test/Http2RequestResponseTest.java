9f05c4cb0a8133acdc5f44d9c3aaba93cbbe27d1
Waqar Ahmed Khan <waahm7@gmail.com>
2023-05-22 14:31:19 -0700

    try same try catch

--- src/test/java/software/amazon/awssdk/crt/test/Http2RequestResponseTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Http2RequestResponseTest.java
@@ -200,10 +200,9 @@ public class Http2RequestResponseTest extends HttpRequestResponseFixture {
                 Http2Request request = getHttp2Request("GET", HOST, "/get", EMPTY_BODY);
                 try (Http2Stream h2Stream = conn.makeRequest(request, streamHandler)) {
                     h2Stream.activate();
-                }
-                try {
                     streamComplete.get();
-                } catch(Exception e) {
+                }
+                catch(Exception e) {
                     // ignoring the exception as it might fail that RST_STREAM has been sent.
                 }
             }

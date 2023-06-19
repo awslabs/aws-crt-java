f7045ff074fe73569f01fb22c4e295e0d29aee83
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-19 14:09:52 -0700

    One test uncomment

--- src/test/java/software/amazon/awssdk/crt/test/Pkcs11LibTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Pkcs11LibTest.java
@@ -38,8 +38,8 @@ public class Pkcs11LibTest extends CrtTestFixture {
         assumeEnvironmentSetUpForPkcs11Tests();
 
         // The published Softhsm package on muslc (Alpine) crashes if we don't use strict finalization
-        try (Pkcs11Lib pkcs11Lib = new Pkcs11Lib(TEST_PKCS11_LIB)) {
-        }
+//        try (Pkcs11Lib pkcs11Lib = new Pkcs11Lib(TEST_PKCS11_LIB)) {
+//        }
     }
 
     @Test

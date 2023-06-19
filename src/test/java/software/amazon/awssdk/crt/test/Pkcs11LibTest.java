f688bc73cdb2010c06af3e422380a77b69581576
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-19 14:41:35 -0700

    Simple strict

--- src/test/java/software/amazon/awssdk/crt/test/Pkcs11LibTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Pkcs11LibTest.java
@@ -38,11 +38,7 @@ public class Pkcs11LibTest extends CrtTestFixture {
         assumeEnvironmentSetUpForPkcs11Tests();
 
         // The published Softhsm package on muslc (Alpine) crashes if we don't use strict finalization
-        try (Pkcs11Lib pkcs11Lib = new Pkcs11Lib(TEST_PKCS11_LIB)) {
-            try (Pkcs11Lib pkcs11Lib2 = new Pkcs11Lib(TEST_PKCS11_LIB)) {
-                try (Pkcs11Lib pkcs11Lib3 = new Pkcs11Lib(TEST_PKCS11_LIB)) {
-                }
-            }
+        try (Pkcs11Lib pkcs11Lib = new Pkcs11Lib(TEST_PKCS11_LIB, Pkcs11Lib.InitializeFinalizeBehavior.STRICT)) {
         }
     }
 

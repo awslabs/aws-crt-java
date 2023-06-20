5b50ec0eec442fdbd3245f789b26e8381c0f8ee4
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-20 09:31:53 -0700

    Enable tests

--- src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
@@ -3128,33 +3128,33 @@ public class Mqtt5ClientTest extends CrtTestFixture {
             AWS_TEST_MQTT5_IOT_CORE_PKCS11_PKEY_LABEL, AWS_TEST_MQTT5_IOT_CORE_PKCS11_CERT_FILE);
 
         // The published Softhsm package on muslc (Alpine) crashes if we don't use strict finalization
-//        try (
-//            Pkcs11Lib pkcs11Lib = new Pkcs11Lib(AWS_TEST_MQTT5_IOT_CORE_PKCS11_LIB, Pkcs11Lib.InitializeFinalizeBehavior.STRICT);
-//            TlsContextPkcs11Options pkcs11Options = new TlsContextPkcs11Options(pkcs11Lib);) {
-//                pkcs11Options.withTokenLabel(AWS_TEST_MQTT5_IOT_CORE_PKCS11_TOKEN_LABEL);
-//                pkcs11Options.withUserPin(AWS_TEST_MQTT5_IOT_CORE_PKCS11_PIN);
-//                pkcs11Options.withPrivateKeyObjectLabel(AWS_TEST_MQTT5_IOT_CORE_PKCS11_PKEY_LABEL);
-//                pkcs11Options.withCertificateFilePath(AWS_TEST_MQTT5_IOT_CORE_PKCS11_CERT_FILE);
-//
-//                LifecycleEvents_Futured events = new LifecycleEvents_Futured();
-//                try (
-//                    TlsContextOptions tlsOptions = TlsContextOptions.createWithMtlsPkcs11(pkcs11Options);
-//                    TlsContext tlsContext = new TlsContext(tlsOptions);
-//                ) {
-//                    Mqtt5ClientOptionsBuilder builder = new Mqtt5ClientOptionsBuilder(AWS_TEST_MQTT5_IOT_CORE_HOST, 8883l);
-//                    builder.withLifecycleEvents(events);
-//                    builder.withTlsContext(tlsContext);
-//
-//                    try (Mqtt5Client client = new Mqtt5Client(builder.build())) {
-//                        client.start();
-//                        events.connectedFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
-//                        DisconnectPacketBuilder disconnect = new DisconnectPacketBuilder();
-//                        client.stop(disconnect.build());
-//                    }
-//                }
-//        } catch (Exception ex) {
-//            fail(ex.getMessage());
-//        }
+        try (
+            Pkcs11Lib pkcs11Lib = new Pkcs11Lib(AWS_TEST_MQTT5_IOT_CORE_PKCS11_LIB, Pkcs11Lib.InitializeFinalizeBehavior.STRICT);
+            TlsContextPkcs11Options pkcs11Options = new TlsContextPkcs11Options(pkcs11Lib);) {
+                pkcs11Options.withTokenLabel(AWS_TEST_MQTT5_IOT_CORE_PKCS11_TOKEN_LABEL);
+                pkcs11Options.withUserPin(AWS_TEST_MQTT5_IOT_CORE_PKCS11_PIN);
+                pkcs11Options.withPrivateKeyObjectLabel(AWS_TEST_MQTT5_IOT_CORE_PKCS11_PKEY_LABEL);
+                pkcs11Options.withCertificateFilePath(AWS_TEST_MQTT5_IOT_CORE_PKCS11_CERT_FILE);
+
+                LifecycleEvents_Futured events = new LifecycleEvents_Futured();
+                try (
+                    TlsContextOptions tlsOptions = TlsContextOptions.createWithMtlsPkcs11(pkcs11Options);
+                    TlsContext tlsContext = new TlsContext(tlsOptions);
+                ) {
+                    Mqtt5ClientOptionsBuilder builder = new Mqtt5ClientOptionsBuilder(AWS_TEST_MQTT5_IOT_CORE_HOST, 8883l);
+                    builder.withLifecycleEvents(events);
+                    builder.withTlsContext(tlsContext);
+
+                    try (Mqtt5Client client = new Mqtt5Client(builder.build())) {
+                        client.start();
+                        events.connectedFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
+                        DisconnectPacketBuilder disconnect = new DisconnectPacketBuilder();
+                        client.stop(disconnect.build());
+                    }
+                }
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        }
     }
 
     /**

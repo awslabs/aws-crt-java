18a1e3af059fa7a02e77ede98c3175f15d54185f
Waqar Ahmed Khan <waahm7@gmail.com>
2023-07-07 09:18:28 -0700

    Update to env.shell.exec

--- .builder/actions/build-classifier.py
+++ .builder/actions/build-classifier.py
@@ -10,12 +10,6 @@ class BuildClassifier(Builder.Action):
 
 
     def run(self, env):
-
-        actions = []
         crt_classifier = os.getenv("CRT_CLASSIFIER")
-        if crt_classifier is not None and os.system(f"mvn -B install -DskipTests -Dshared-lib.skip=true -Dcrt.classifier={crt_classifier}"):
-
-            # Failed
-            actions.append("exit 1")
-
-        return Builder.Script(actions, name='build-classifier')
+        if crt_classifier is not None:
+            env.shell.exec("mvn", "-B", "install", "-DskipTests", "-Dshared-lib.skip=true", f"-Dcrt.classifier={crt_classifier}", check=True)

65a637a3bfe2e4c9a0d55672e784f914a7ecc724
Steve Kim <sbstevek@amazon.com>
2023-08-11 16:21:05 -0700

    testapp build

---
+++ android/testapp/src/main/java/software/amazon/awssdk/crttest/MainActivity.kt
@@ -0,0 +1,11 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+ package software.amazon.awssdk.crttest
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+}

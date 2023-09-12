c13fbe7d169bebb00602300d47b70c8cdf83b871
Zhihui Xia <zhvxia@amazon.com>
2023-09-12 10:51:32 -0700

    quick test

--- src/main/java/software/amazon/awssdk/crt/mqtt/MqttClientConnection.java
+++ src/main/java/software/amazon/awssdk/crt/mqtt/MqttClientConnection.java
@@ -141,17 +141,6 @@ public class MqttClientConnection extends CrtResource {
             }
             connectAck = null;
         }
-
-        MqttClientConnectionEvents callbacks = config.getConnectionCallbacks();
-        if (callbacks != null) {
-            if (errorCode == 0) {
-                OnConnectionSuccessReturn returnData = new OnConnectionSuccessReturn(sessionPresent);
-                callbacks.onConnectionSuccess(returnData);
-            } else {
-                OnConnectionFailureReturn returnData = new OnConnectionFailureReturn(errorCode);
-                callbacks.onConnectionFailure(returnData);
-            }
-        }
     }
 
     // Called when the connection drops or is disconnected. If errorCode == 0, the

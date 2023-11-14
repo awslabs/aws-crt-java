80e3136bb3e77ef40f30271c5471d6523ca2e191
Zhihui Xia <zhvxia@amazon.com>
2023-11-14 09:54:53 -0800

    disable log for testing

--- utils/Canary/src/main/java/canary/mqtt5/Mqtt5Canary.java
+++ utils/Canary/src/main/java/canary/mqtt5/Mqtt5Canary.java
@@ -197,15 +197,15 @@ public class Mqtt5Canary {
 
     static void PrintLog(String message) {
         return;
-        if (configLogStdout == true) {
-            System.out.println("[CANARY] " + message);
-        }
-        if (configFilePrinter != null) {
-            configFilePrinter.println(message);
-        }
-        if (configLogAWS == true) {
-            Log.log(configLogAWSLevel, LogSubject.MqttClient, "[CANARY] " + message);
-        }
+        // if (configLogStdout == true) {
+        //     System.out.println("[CANARY] " + message);
+        // }
+        // if (configFilePrinter != null) {
+        //     configFilePrinter.println(message);
+        // }
+        // if (configLogAWS == true) {
+        //     Log.log(configLogAWSLevel, LogSubject.MqttClient, "[CANARY] " + message);
+        // }
     }
 
     static void exitWithError(int errorCode) {

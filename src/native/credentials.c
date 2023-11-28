059730ad305fccdafdc2ff44f08d35a2241abe0a
Dengke Tang <dengket@amazon.com>
2023-11-28 07:55:42 -0800

    Performance optimization Updates (#728)
    Co-authored-by: Waqar Ahmed Khan <waahm7@gmail.com>

--- src/native/credentials.c
+++ src/native/credentials.c
@@ -32,6 +32,9 @@ struct aws_credentials *aws_credentials_new_from_java_credentials(JNIEnv *env, j
     jbyteArray session_token =
         (*env)->GetObjectField(env, java_credentials, credentials_properties.session_token_field_id);
 
+    jlong expiration_timepoint_secs =
+        (*env)->GetLongField(env, java_credentials, credentials_properties.expiration_field_id);
+
     if (access_key_id == NULL && secret_access_key == NULL) {
         return aws_credentials_new_anonymous(aws_jni_get_allocator());
     }
@@ -58,7 +61,11 @@ struct aws_credentials *aws_credentials_new_from_java_credentials(JNIEnv *env, j
     }
 
     credentials = aws_credentials_new(
-        aws_jni_get_allocator(), access_key_id_cursor, secret_access_key_cursor, session_token_cursor, UINT64_MAX);
+        aws_jni_get_allocator(),
+        access_key_id_cursor,
+        secret_access_key_cursor,
+        session_token_cursor,
+        (uint64_t)expiration_timepoint_secs);
 
     aws_jni_byte_cursor_from_jbyteArray_release(env, access_key_id, access_key_id_cursor);
     aws_jni_byte_cursor_from_jbyteArray_release(env, secret_access_key, secret_access_key_cursor);
@@ -67,6 +74,59 @@ struct aws_credentials *aws_credentials_new_from_java_credentials(JNIEnv *env, j
     }
 
     return credentials;
+}
+
+jobject aws_java_credentials_from_native_new(JNIEnv *env, const struct aws_credentials *credentials) {
+
+    jobject java_credentials = NULL;
+    jbyteArray access_key_id = NULL;
+    jbyteArray secret_access_key = NULL;
+    jbyteArray session_token = NULL;
+    java_credentials =
+        (*env)->NewObject(env, credentials_properties.credentials_class, credentials_properties.constructor_method_id);
+    if (java_credentials != NULL) {
+
+        struct aws_byte_cursor access_key_id_cursor = aws_credentials_get_access_key_id(credentials);
+        if (access_key_id_cursor.len > 0) {
+            access_key_id = aws_jni_byte_array_from_cursor(env, &access_key_id_cursor);
+        }
+
+        struct aws_byte_cursor secret_access_key_cursor = aws_credentials_get_secret_access_key(credentials);
+        if (secret_access_key_cursor.len > 0) {
+            secret_access_key = aws_jni_byte_array_from_cursor(env, &secret_access_key_cursor);
+        }
+
+        struct aws_byte_cursor session_token_cursor = aws_credentials_get_session_token(credentials);
+        if (session_token_cursor.len > 0) {
+            session_token = aws_jni_byte_array_from_cursor(env, &session_token_cursor);
+        }
+
+        (*env)->SetObjectField(env, java_credentials, credentials_properties.access_key_id_field_id, access_key_id);
+        (*env)->SetObjectField(
+            env, java_credentials, credentials_properties.secret_access_key_field_id, secret_access_key);
+        if (session_token != NULL) {
+            (*env)->SetObjectField(env, java_credentials, credentials_properties.session_token_field_id, session_token);
+        }
+        (*env)->SetLongField(
+            env,
+            java_credentials,
+            credentials_properties.expiration_field_id,
+            (jlong)aws_credentials_get_expiration_timepoint_seconds(credentials));
+    } else {
+        return NULL;
+    }
+
+    if (access_key_id) {
+        (*env)->DeleteLocalRef(env, access_key_id);
+    }
+    if (secret_access_key) {
+        (*env)->DeleteLocalRef(env, secret_access_key);
+    }
+    if (session_token) {
+        (*env)->DeleteLocalRef(env, session_token);
+    }
+
+    return java_credentials;
 }
 
 #if UINTPTR_MAX == 0xffffffff

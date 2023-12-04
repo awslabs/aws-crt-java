45740a74d9bd46b008f3f6331ed6dd195bedbfcd
Steve Kim <86316075+sbSteveK@users.noreply.github.com>
2023-12-04 09:33:01 -0800

    Android architecture detection (#718)

--- android/crt/src/main/java/software/amazon/awssdk/crt/android/CrtPlatformImpl.java
+++ android/crt/src/main/java/software/amazon/awssdk/crt/android/CrtPlatformImpl.java
@@ -10,6 +10,7 @@ import software.amazon.awssdk.crt.BuildConfig;
 import software.amazon.awssdk.crt.CrtPlatform;
 import software.amazon.awssdk.crt.utils.PackageInfo;
 import java.util.Locale;
+import android.os.Build;
 
 public class CrtPlatformImpl extends CrtPlatform {
     public String getOSIdentifier() {
@@ -28,8 +29,9 @@ public class CrtPlatformImpl extends CrtPlatform {
     }
 
     public String getResourcePath(String cRuntime, String libraryName) {
-        // Internal folder structure of Android aar libraries are different from jar libraries
-        String arch = normalize(System.getProperty("os.arch"));
+        // Internal folder structure of Android aar libraries are different from jar libraries and arch must
+        // be retrieved using Build class instead of a system property.
+        String arch = Build.CPU_ABI;
 
         if (arch.matches("^(x8664|amd64|ia32e|em64t|x64|x86_64)$")) {
             arch = "x86_64";
@@ -41,7 +43,7 @@ public class CrtPlatformImpl extends CrtPlatform {
             } else {
                 throw new RuntimeException("AWS CRT: architecture not supported on Android: " + arch);
             }
-        } else if (arch.startsWith("arm64") || arch.startsWith("aarch64")) {
+        } else if (arch.startsWith("arm64") || arch.startsWith("aarch64") || arch.equals("armv8a")) {
             arch =  "arm64-v8a";
         } else if (arch.equals("armv7l")) {
             arch =  "armeabi-v7a";

d059836e977f559a2fa4232bea8f608613d7a8a2
Dengke <dengket@amazon.com>
2024-08-30 16:33:04 -0700

    move it to internal

---
+++ src/main/java/software/amazon/awssdk/crt/internal/NativeFeature.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+package software.amazon.awssdk.crt.internal;
+
+import org.graalvm.nativeimage.hosted.Feature;
+
+import software.amazon.awssdk.crt.CRT;
+
+/**
+ * Implementation of GraalVM feature to extract the share lib to the image path.
+ * Internal API, not for external usage.
+ */
+public class NativeFeature implements Feature {
+
+    @Override
+    public void afterImageWrite(AfterImageWriteAccess access) {
+      new CRT();
+      CRT.extractLibrary(access.getImagePath().getParent().toString());
+    }
+}

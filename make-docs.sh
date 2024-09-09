814357b003c5c90dfb1029b2dbbd1fd6804cd348
Dengke <dengket@amazon.com>
2024-09-09 13:45:13 -0700

    update comments

--- make-docs.sh
+++ make-docs.sh
@@ -9,7 +9,7 @@ rm -rf docs/
 
 # build
 mvn clean javadoc:javadoc -Prelease
-# mvn generates the doc in apidocs/ subfolder, move it out
+# mvn generates the doc in target/site/apidocs/ by default, move it to our common doc folder
 cp -r target/site/apidocs/ docs/
 
 popd >/dev/null

cf326f39aca78c7724924069f35a236a6ebcef2b
Michael Graeb <graebm@amazon.com>
2024-09-10 17:53:13 -0700

    docs tweaks - move javadoc settings to be global, not just in the "release" profile - move cmake-configure out of the "generate-sources" phase, so it doesn't run during javadoc generation (yay faster iteration)

--- make-docs.sh
+++ make-docs.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-pushd $(dirname $0) >/dev/null
+pushd $(dirname $0) > /dev/null
 
 # clean
 rm -rf docs/
 
 # build
-mvn clean javadoc:javadoc -Prelease
-# mvn generates the doc in target/site/apidocs/ by default, move it to our common doc folder
+mvn javadoc:javadoc -Dmaven.javadoc.failOnWarnings=true
+
+# copy to docs/
 cp -r target/site/apidocs/ docs/
 
-popd >/dev/null
+popd > /dev/null

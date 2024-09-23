a5b2cf10b5d2ede342ca78dba445277c87d80bed
DmitriyMusatkin <musatkd@amazon.com>
2024-09-23 15:41:02 -0700

    compile fix

--- src/native/checksums.c
+++ src/native/checksums.c
@@ -17,7 +17,10 @@ jint crc32_common(
     size_t length,
     uint32_t (*checksum_fn)(const uint8_t *, size_t, uint32_t)) {
     struct aws_byte_cursor c_byte_array = aws_jni_byte_cursor_from_jbyteArray_acquire(env, input);
-    jint res_signed = (jint)checksum_fn(c_byte_array.ptr, c_byte_array.len, 0);
+    struct aws_byte_cursor cursor = c_byte_array;
+    aws_byte_cursor_advance(&cursor, start);
+    cursor.len = aws_min_size(length, cursor.len);
+    jint res_signed = (jint)checksum_fn(cursor.ptr, cursor.len, previous);
     aws_jni_byte_cursor_from_jbyteArray_release(env, input, c_byte_array);
     return res_signed;
 }
@@ -59,7 +62,10 @@ JNIEXPORT jint JNICALL Java_software_amazon_awssdk_crt_checksums_CRC64NVME_crc64
     aws_cache_jni_ids(env);
 
     struct aws_byte_cursor c_byte_array = aws_jni_byte_cursor_from_jbyteArray_acquire(env, input);
-    jint res_signed = (jint)aws_checksums_crc64nvme_ex(c_byte_array.ptr, c_byte_array.len, 0);
+    struct aws_byte_cursor cursor = c_byte_array;
+    aws_byte_cursor_advance(&cursor, offset);
+    cursor.len = aws_min_size(length, cursor.len);
+    jint res_signed = (jint)aws_checksums_crc64nvme_ex(cursor.ptr, cursor.len, previous);
     aws_jni_byte_cursor_from_jbyteArray_release(env, input, c_byte_array);
     return res_signed;
 }

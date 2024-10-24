de2eb6f34aa8cefb7a37ed0c12db63c7a264bb0a
Waqar Ahmed Khan <waahm7@gmail.com>
2024-10-24 11:39:21 -0700

    fix deprecated API

--- android/crt/src/main/java/software/amazon/awssdk/crt/android/CrtPlatformImpl.java
+++ android/crt/src/main/java/software/amazon/awssdk/crt/android/CrtPlatformImpl.java
@@ -28,29 +28,33 @@ public class CrtPlatformImpl extends CrtPlatform {
         return value.toLowerCase(Locale.US).replaceAll("[^a-z0-9]+", "");
     }
 
-    public String getResourcePath(String cRuntime, String libraryName) {
+
+    public String getArch() {
         // Internal folder structure of Android aar libraries are different from jar libraries and arch must
         // be retrieved using Build class instead of a system property.
-        String arch = Build.CPU_ABI;
+        for (String arch: Build.SUPPORTED_ABIS) {
+            if (arch.matches("^(x8664|amd64|ia32e|em64t|x64|x86_64)$")) {
+                return "x86_64";
+            } else if (arch.matches("^(x8632|x86|i[3-6]86|ia32|x32)$")) {
+                return "x86";
+            } else if (arch.startsWith("armeabi")) {
+                if (arch.contains("v7")) {
+                    return "armeabi-v7a";
+                } 
+            } else if (arch.startsWith("arm64") || arch.startsWith("aarch64") || arch.equals("armv8a")) {
+                 return "arm64-v8a";
+            } else if (arch.equals("armv7l")) {
+                return "armeabi-v7a";
+            } 
+        }
+        return null;
+    }
 
-        if (arch.matches("^(x8664|amd64|ia32e|em64t|x64|x86_64)$")) {
-            arch = "x86_64";
-        } else if (arch.matches("^(x8632|x86|i[3-6]86|ia32|x32)$")) {
-            arch =  "x86";
-        } else if (arch.startsWith("armeabi")) {
-            if (arch.contains("v7")) {
-                arch =  "armeabi-v7a";
-            } else {
-                throw new RuntimeException("AWS CRT: architecture not supported on Android: " + arch);
-            }
-        } else if (arch.startsWith("arm64") || arch.startsWith("aarch64") || arch.equals("armv8a")) {
-            arch =  "arm64-v8a";
-        } else if (arch.equals("armv7l")) {
-            arch =  "armeabi-v7a";
-        } else {
+    public String getResourcePath(String cRuntime, String libraryName) {
+        String arch = getArch(); 
+        if(arch == null) {
             throw new RuntimeException("AWS CRT: architecture not supported on Android: " + arch);
         }
-
         return "/lib/" + arch + "/" + libraryName;
     }
 

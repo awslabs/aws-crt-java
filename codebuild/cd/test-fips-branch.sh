f6e17b8511c2dc35d4b8dc5e06c0a2f1b3efca07
Dengke <dengket@amazon.com>
2024-12-11 16:24:00 -0800

    what? they now use main branch??

--- codebuild/cd/test-fips-branch.sh
+++ codebuild/cd/test-fips-branch.sh
@@ -9,12 +9,12 @@ cd ./crt/aws-lc || exit 1
 # Get the current commit hash
 current_commit=$(git rev-parse HEAD)
 
-# Check if the current commit is from the "fips-2024-09-27" branch
-if git merge-base --is-ancestor "$current_commit" "origin/fips-2024-09-27"; then
-    echo "Current aws-lc commit is from the 'fips-2024-09-27' branch"
+# Check if the current commit is from the "main" branch
+if git merge-base --is-ancestor "$current_commit" "origin/main"; then
+    echo "Current aws-lc commit is from the 'main' branch"
     status=0
 else
-    echo "Error: Current aws-lc commit is not from the 'fips-2024-09-27' branch"
+    echo "Error: Current aws-lc commit is not from the 'main' branch"
     status=1
 fi
 

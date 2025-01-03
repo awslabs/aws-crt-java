87d667592ed482416d08c64ce4af877191a16f2f
DmitriyMusatkin <musatkd@amazon.com>
2025-01-03 11:23:12 -0800

    debug info

--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -18,7 +18,10 @@ export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:$
 export QEMU_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/multiarch-qemu-user-static:latest
 
 docker run --rm --privileged ${QEMU_IMAGE} --reset -p yes
+ls -la /proc/sys/fs/binfmt_misc
+
 docker run --rm --privileged ${QEMU_IMAGE} --reset -p yes
+ls -la /proc/sys/fs/binfmt_misc
 
 export BRANCH_TAG=$(git describe --tags)
 docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false

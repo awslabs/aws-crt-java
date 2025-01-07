58b731f0d26c119995bdb61f682f7a1903d1b03b
DmitriyMusatkin <musatkd@amazon.com>
2025-01-07 14:27:53 -0800

    what if we add arch

--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -8,6 +8,8 @@ CLASSIFIER=$1
 shift
 PLATFORM=$1
 shift
+ARCH=$1
+shift
 
 # Pry the builder version this CRT is using out of ci.yml
 BUILDER_VERSION=$(cat .github/workflows/ci.yml | grep 'BUILDER_VERSION:' | sed 's/\s*BUILDER_VERSION:\s*\(.*\)/\1/')
@@ -17,7 +19,7 @@ aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazona
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
 
 export BRANCH_TAG=$(git describe --tags)
-docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
+docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --target linux-${ARCH} --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
 docker container prune -f
 
 # Upload the artifacts to S3

d2cc9a1837e71f4de99748735a50f58452536703
DmitriyMusatkin <musatkd@amazon.com>
2025-01-07 13:38:24 -0800

    no qemu

--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -15,12 +15,9 @@ echo "Using builder version ${BUILDER_VERSION}"
 
 aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
-export QEMU_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/multiarch-qemu-user-static:latest
+#export QEMU_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/multiarch-qemu-user-static:latest
 
-docker run --rm --privileged ${QEMU_IMAGE} --reset -p yes
-ls -la /proc/sys/fs/binfmt_misc
-
-docker run --rm -t --platform=aarch64 arm64v8/ubuntu uname -m
+#docker run --rm --privileged ${QEMU_IMAGE} --reset -p yes
 
 export BRANCH_TAG=$(git describe --tags)
 docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false

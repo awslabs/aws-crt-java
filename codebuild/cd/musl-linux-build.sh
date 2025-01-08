79296c34dcb4d557f90f524b7231680859779c4a
DmitriyMusatkin <musatkd@amazon.com>
2025-01-08 11:30:16 -0800

    qus

--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -18,6 +18,8 @@ echo "Using builder version ${BUILDER_VERSION}"
 aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
 
+docker run --rm --privileged aptman/qus -s -- -p arm32v7
+
 export BRANCH_TAG=$(git describe --tags)
 docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --target alpine-${ARCH} --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
 docker container prune -f

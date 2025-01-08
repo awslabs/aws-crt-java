1d16033e2f181ff2966d3aa842c46424b94ad2a3
DmitriyMusatkin <musatkd@amazon.com>
2025-01-07 16:24:44 -0800

    alpine

--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -19,7 +19,7 @@ aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazona
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
 
 export BRANCH_TAG=$(git describe --tags)
-docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --target linux-${ARCH} --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
+docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH --platform=${PLATFORM} $DOCKER_IMAGE --version=${BUILDER_VERSION} build -p aws-crt-java --target alpine-${ARCH} --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false
 docker container prune -f
 
 # Upload the artifacts to S3

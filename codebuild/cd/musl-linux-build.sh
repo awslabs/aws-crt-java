16900d76d67cbe65773342d8505e8b7751e08c61
DmitriyMusatkin <musatkd@amazon.com>
2025-01-13 15:38:39 -0800

    simplify

--- codebuild/cd/musl-linux-build.sh
+++ codebuild/cd/musl-linux-build.sh
@@ -16,12 +16,11 @@ echo "Using builder version ${BUILDER_VERSION}"
 aws ecr get-login-password | docker login 123124136734.dkr.ecr.us-east-1.amazonaws.com -u AWS --password-stdin
 export DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/${IMAGE_NAME}:${BUILDER_VERSION}
 
-# on x86-64 and aarch64 we run on native images in codebuild
+# on x86-64 and aarch64 we run on native images in codebuild 
+# (note: we still register mapping for those platforms, but in practice they are not used)
 # on other platforms we require emulation which is done through binfmt
-if [ "$ARCH" != "linux/aarch64" ] && [ "$ARCH" != "linux/amd64" ]; then
-  export QEMU_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/multiarch-qemu-user-static:latest
-  docker run --rm --privileged ${QEMU_IMAGE} --reset -p yes
-fi
+export QEMU_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/multiarch-qemu-user-static:latest
+docker run --rm --privileged ${QEMU_IMAGE} --reset -p yes
 
 export BRANCH_TAG=$(git describe --tags)
 docker run --mount type=bind,src=$(pwd),dst=/root/aws-crt-java --env AWS_DEFAULT_REGION --env CXXFLAGS --env AWS_CRT_ARCH $DOCKER_IMAGE --platform =${ARCH} --version=${BUILDER_VERSION} build -p aws-crt-java --classifier ${CLASSIFIER} --branch ${BRANCH_TAG} run_tests=false

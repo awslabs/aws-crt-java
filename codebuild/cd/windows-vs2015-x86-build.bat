038c3d4239e08a1395bfe734e823e908d9c2bdae
Vera Xia <zhvxia@amazon.com>
2025-06-03 09:27:56 -0700

    use vs 15 2017

--- codebuild/cd/windows-vs2015-x86-build.bat
+++ codebuild/cd/windows-vs2015-x86-build.bat
@@ -5,7 +5,12 @@ echo PATH=%PATH%
 echo JAVA_HOME=%JAVA_HOME%
 
 set AWS_CRT_WINDOWS_SDK_VERSION=10.0.17763.0
-set AWS_CMAKE_GENERATOR=Visual Studio 14 2015
+@REM The last Windows 10 SDK version that VS 2015 can target is 10.0.14393.0. 
+@REM To target 10.0.17763.0, we need to use the VS 2017 and later generator.
+@REM "VS 2015 Users: The Windows 10 SDK (15063, 16299, 17134, 17763) is
+@REM officially only supported for VS 2017." From:
+@REM https://blogs.msdn.microsoft.com/chuckw/2018/10/02/windows-10-october-2018-update/
+set AWS_CMAKE_GENERATOR=Visual Studio 15 2017
 
 git submodule update --init
 

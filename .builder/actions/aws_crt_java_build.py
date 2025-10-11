b1da357b09c165451c74833905555893f51a2247
Dengke Tang <dengket@amazon.com>
2025-10-10 17:01:20 -0700

    fix code pipeline

--- .builder/actions/aws_crt_java_build.py
+++ .builder/actions/aws_crt_java_build.py
@@ -8,10 +8,10 @@ class AWSCrtJavaBuild(Builder.Action):
 
     def run(self, env):
         if os.getenv("CRT_FIPS") is not None:
-            env.shell.exec("mvn", "-P", "continuous-integration", "-B", "compile",
+            env.shell.exec("mvn", "-X", "-Dmaven.wagon.http.retryHandler.class=standard", "-P", "continuous-integration", "-B", "compile",
                            "-Dcmake.crt_fips=ON", check=True)
         else:
-            env.shell.exec("mvn", "-P", "continuous-integration",
+            env.shell.exec("mvn", "-X", "-Dmaven.wagon.http.retryHandler.class=standard", "-P", "continuous-integration",
                            "-B", "compile", check=True)
 
         parser = argparse.ArgumentParser()

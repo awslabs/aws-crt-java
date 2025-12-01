fcb28a044b12a3f47301cd8db97d85f9b853e25f
DmitriyMusatkin <musatkd@amazon.com>
2025-12-01 13:13:40 -0800

    print args

--- src/test/java/software/amazon/awssdk/crt/test/CrcTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/CrcTest.java
@@ -8,6 +8,9 @@ package software.amazon.awssdk.crt.test;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class CrcTest extends CrtTestFixture {
     public CrcTest() {
     }
@@ -212,5 +215,68 @@ public class CrcTest extends CrtTestFixture {
         }
         long expected = 0xB9D9D4A8492CBD7FL;
         assertEquals(expected, crc64.getValue());
+    }
+
+    @Test
+    public void testCrc64NVMEBench() throws NoSuchAlgorithmException {
+        int data_size = 128  * 1024;
+        int num_iter = 100;
+
+        long crc64_oneshot_duration_sum = 0;
+        long crc64_multi_duration_sum = 0;
+
+        for (int iter = 0; iter < num_iter; ++iter) {
+            long startTime2 = System.currentTimeMillis();
+
+            software.amazon.awssdk.crt.checksums.CRC64NVME crc642 = new software.amazon.awssdk.crt.checksums.CRC64NVME();
+            byte[] values2 = new byte[4000 * data_size];
+            crc642.update(values2);
+
+            long final_val2 = crc642.getValue();
+
+            long endTime2 = System.currentTimeMillis();
+            long one_shot_time = endTime2 - startTime2;
+            crc64_oneshot_duration_sum += one_shot_time;
+
+
+            byte[] values = new byte[data_size];
+            for (int i = 0; i < data_size; i++) {
+                values[i] = (byte)(i % 255);
+            }
+
+            long startTime = System.currentTimeMillis();
+
+            software.amazon.awssdk.crt.checksums.CRC64NVME crc64 = new software.amazon.awssdk.crt.checksums.CRC64NVME();
+            for (int i = 0; i < 4000; i++) {
+                crc64.update(values, 0, data_size);
+            }
+
+            long final_val = crc64.getValue();
+
+            long endTime = System.currentTimeMillis();
+            long update_time = endTime - startTime;
+            crc64_multi_duration_sum += update_time;
+
+             System.out.println("crc64 iter update: " + update_time + " oneshot: " + one_shot_time);
+        }
+
+        System.out.println("crc64 oneshot avg time: " + (double)crc64_oneshot_duration_sum / num_iter);
+        System.out.println("crc64 multi avg time: " + (double)crc64_multi_duration_sum / num_iter);
+
+
+        /*
+        long startTimeSha256 = System.currentTimeMillis();
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        for (int i = 0; i < 1_000; i++) {
+            sha256.update(values, 0, data_size);
+        }
+
+        byte[] final_sha = sha256.digest();
+
+        long endTimeSha256 = System.currentTimeMillis();
+        long durationSha256 = endTimeSha256 - startTimeSha256;
+
+        System.out.println("sha256 time: " + durationSha256);
+        */
     }
 }

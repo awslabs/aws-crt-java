90bc1c68d61fbb1e10a59569cb69534b69ba0300
Krish <>
2025-12-08 11:00:33 -0800

    test fail and ci fail

--- src/test/java/software/amazon/awssdk/crt/test/FailFastListener.java
+++ src/test/java/software/amazon/awssdk/crt/test/FailFastListener.java
@@ -6,6 +6,6 @@ import org.junit.runner.notification.RunListener;
 public class FailFastListener extends RunListener {
     public void testFailure(Failure failure) throws Exception {
         System.err.println("FAILURE: " + failure);
-        System.exit(-1);
+        System.exit(1);
     }
 }

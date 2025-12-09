9fa0cd59a08f3d8cca0af4744606072c7dbd4c76
Krish <>
2025-12-09 15:58:35 -0800

    add a comment

--- src/test/java/software/amazon/awssdk/crt/test/FailFastListener.java
+++ src/test/java/software/amazon/awssdk/crt/test/FailFastListener.java
@@ -6,6 +6,8 @@ import org.junit.runner.notification.RunListener;
 public class FailFastListener extends RunListener {
     public void testFailure(Failure failure) throws Exception {
         System.err.println("FAILURE: " + failure);
+        // Previously we used a negative code but caused CI to pass,
+        // for unknown reasons, even when tests failed on Windows.
         System.exit(1);
     }
 }

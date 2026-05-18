0df8b4d5b3b78bdc0a39bb77f365821dd0dca865
DmitriyMusatkin <musatkd@amazon.com>
2026-05-18 16:33:03 -0700

    fix test

--- src/test/java/software/amazon/awssdk/crt/test/CredentialsProviderTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/CredentialsProviderTest.java
@@ -25,6 +25,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
+import software.amazon.awssdk.crt.Log;
+
 import static org.junit.Assert.*;
 import static org.junit.Assert.fail;
 
@@ -289,6 +291,7 @@ public class CredentialsProviderTest extends CrtTestFixture {
 
     @Test
     public void testCreateDestroyProfile_MissingCreds() throws ExecutionException, InterruptedException, IOException {
+         Log.initLoggingToStderr(Log.LogLevel.Trace);
         Path confPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_conf_", "");
         Path credsPath = Files.createTempFile("testCreateDestroyProfile_ValidProfile_creds_", "");
         Files.write(credsPath, Arrays.asList("[default]")); // Contains a section header but no actual credentials
@@ -297,18 +300,18 @@ public class CredentialsProviderTest extends CrtTestFixture {
                 .withConfigFileNameOverride(confPath.toString()).withCredentialsFileNameOverride(credsPath.toString());
 
         try (ProfileCredentialsProvider provider = builder.build()) {
-            assertNotNull(provider);
+            assertNotNull("provider is null", provider);
             assertTrue(provider.getNativeHandle() != 0);
 
             try {
                 provider.getCredentials().join();
                 fail("Expected credential fetching to throw an exception since creds are missing from profile");
             } catch (CompletionException e) {
-                assertNotNull(e.getCause());
+                assertNotNull("exception is null", e.getCause());
                 Throwable innerException = e.getCause();
 
                 // Check that the right exception type caused the completion error in the future
-                assertTrue(innerException.getMessage().contains("Parser encountered an error"));
+                assertEquals(innerException.getMessage(), "Valid credentials could not be sourced by a profile provider");
                 assertEquals(CrtRuntimeException.class, innerException.getClass());
             }
         } finally {

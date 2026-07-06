ca7cbb960c2c8b20909c5ca2527f43a6c9617804
Igor Abdrakhimov <igorabd@amazon.com>
2026-07-06 11:42:02 -0700

    Use single client for auto-PUBACK test
    Self-publish instead of using separate publisher/subscriber clients.

--- src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
+++ src/test/java/software/amazon/awssdk/crt/test/Mqtt5ClientTest.java
@@ -2644,23 +2644,16 @@ public class Mqtt5ClientTest extends Mqtt5ClientTestFixture {
             CompletableFuture<byte[]> firstDeliveryFuture = new CompletableFuture<>();
             CompletableFuture<byte[]> unexpectedRedeliveryFuture = new CompletableFuture<>();
 
-            // Publisher
-            Mqtt5ClientOptionsBuilder publisherBuilder = new Mqtt5ClientOptionsBuilder(AWS_TEST_MQTT5_IOT_CORE_HOST, 8883l);
-            LifecycleEvents_Futured publisherEvents = new LifecycleEvents_Futured();
-            publisherBuilder.withLifecycleEvents(publisherEvents);
-            publisherBuilder.withTlsContext(tlsContext);
-
-            // Subscriber - uses default auto-PUBACK behavior (no acquirePublishAcknowledgementControl)
-            Mqtt5ClientOptionsBuilder subscriberBuilder = new Mqtt5ClientOptionsBuilder(AWS_TEST_MQTT5_IOT_CORE_HOST, 8883l);
-            LifecycleEvents_Futured subscriberEvents = new LifecycleEvents_Futured();
-            subscriberBuilder.withLifecycleEvents(subscriberEvents);
-            subscriberBuilder.withTlsContext(tlsContext);
+            Mqtt5ClientOptionsBuilder clientBuilder = new Mqtt5ClientOptionsBuilder(AWS_TEST_MQTT5_IOT_CORE_HOST, 8883l);
+            LifecycleEvents_Futured events = new LifecycleEvents_Futured();
+            clientBuilder.withLifecycleEvents(events);
+            clientBuilder.withTlsContext(tlsContext);
 
             ConnectPacketBuilder connectOptions = new ConnectPacketBuilder();
             connectOptions.withClientId("test/MQTT5_AutoPuback_NoDuplicate_Java_" + testUUID);
-            subscriberBuilder.withConnectOptions(connectOptions.build());
+            clientBuilder.withConnectOptions(connectOptions.build());
 
-            subscriberBuilder.withPublishEvents(new Mqtt5ClientOptions.PublishEvents() {
+            clientBuilder.withPublishEvents(new Mqtt5ClientOptions.PublishEvents() {
                 @Override
                 public void onMessageReceived(Mqtt5Client client, PublishReturn publishReturn) {
                     byte[] receivedPayload = publishReturn.getPublishPacket().getPayload();
@@ -2674,20 +2667,17 @@ public class Mqtt5ClientTest extends Mqtt5ClientTestFixture {
                 }
             });
 
-            try (Mqtt5Client publisher = new Mqtt5Client(publisherBuilder.build());
-                 Mqtt5Client subscriber = new Mqtt5Client(subscriberBuilder.build())) {
-                publisher.start();
-                publisherEvents.connectedFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
-                subscriber.start();
-                subscriberEvents.connectedFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
+            try (Mqtt5Client client = new Mqtt5Client(clientBuilder.build())) {
+                client.start();
+                events.connectedFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
 
                 // Subscribe to the topic with QoS 1
                 SubscribePacketBuilder subscribeBuilder = new SubscribePacketBuilder(testTopic, QOS.AT_LEAST_ONCE);
-                subscriber.subscribe(subscribeBuilder.build()).get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
+                client.subscribe(subscribeBuilder.build()).get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
 
                 // Publish a QoS 1 message
                 PublishPacketBuilder publishBuilder = new PublishPacketBuilder(testTopic, QOS.AT_LEAST_ONCE, payload);
-                publisher.publish(publishBuilder.build()).get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
+                client.publish(publishBuilder.build()).get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
 
                 // Wait for the first delivery
                 byte[] firstPayload = firstDeliveryFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
@@ -2704,10 +2694,8 @@ public class Mqtt5ClientTest extends Mqtt5ClientTestFixture {
                 assertTrue("Auto-PUBACK should have been sent (no acquirePublishAcknowledgementControl called), verified by absence of re-delivery",
                         !redelivered);
 
-                subscriber.stop();
-                subscriberEvents.stopFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
-                publisher.stop();
-                publisherEvents.stopFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
+                client.stop();
+                events.stopFuture.get(OPERATION_TIMEOUT_TIME, TimeUnit.SECONDS);
             }
         } catch (Exception ex) {
             throw new RuntimeException(ex);
